65505a14e1609b300a9eec1267c6c80b1ec6ba2f
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2019-05-24 17:49:31 +0200

    Tests for Zygote and ForwardDiff

---
+++ test/testAD.jl
@@ -0,0 +1,61 @@
+using Zygote, ForwardDiff
+
+dims = [10,5]
+
+A = rand(dims...)
+B = rand(dims...)
+K = [zeros(dims[1],dims[1]),zeros(dims[2],dims[2])]
+kernels = [SquaredExponentialKernel]
+l = 2.0
+vl = l*ones(dims[1])
+testfunction(k,A,B) = sum(kernelmatrix(k,A,B))
+testfunction(k,A) = sum(kernelmatrix(k,A))
+
+
+#For debugging
+Zygote.gradient(x->testfunction(SquaredExponentialKernel(x),A,B),vl)
+Zygote.gradient(x->testfunction(SquaredExponentialKernel(x),A,B),vl)
+Zygote.gradient(x->testfunction(SquaredExponentialKernel(x),A,B),l)
+ForwardDiff.gradient(x->testfunction(SquaredExponentialKernel(x[1]),A),[l])
+
+##Eventually store real results in file
+
+@testset "Zygote Automatic Differentiation test" begin
+    @testset "ARD" begin
+        for k in kernels
+            @test Zygote.gradient(x->testfunction(k(x),A,B),vl)
+            @test Zygote.gradient(x->testfunction(k(vl),x,B),A)
+            @test Zygote.gradient(x->testfunction(k(x),A),vl)
+            @test Zygote.gradient(x->testfunction(k(vl),x),A)
+        end
+    end
+    @testset "ISO" begin
+        for k in kernels
+            @test Zygote.gradient(x->testfunction(k(x),A,B),l)
+            @test Zygote.gradient(x->testfunction(k(l),x,B),A)
+            @test Zygote.gradient(x->testfunction(k(x),A),l)
+            @test Zygote.gradient(x->testfunction(k(l),x),A)
+
+        end
+    end
+end
+
+@testset "ForwardDiff AutomaticDifferentation test" begin
+    @testset "ARD" begin
+        for k in kernels
+            @test ForwardDiff.gradient(x->testfunction(k(x),A,B),vl)
+            @test ForwardDiff.gradient(x->testfunction(k(vl),x,B),A)
+            @test ForwardDiff.gradient(x->testfunction(k(x),A),vl)
+            @test ForwardDiff.gradient(x->testfunction(k(vl),x),A)
+        end
+    end
+    @testset "ISO" begin
+        for k in kernels
+            @test ForwardDiff.gradient(x->testfunction(k(x[1]),A,B),[l])
+            @test ForwardDiff.gradient(x->testfunction(k(l),x,B),A)
+            @test ForwardDiff.gradient(x->testfunction(k(x),A),[l])
+            @test ForwardDiff.gradient(x->testfunction(k(l[1]),x),A)
+
+        end
+    end
+end
