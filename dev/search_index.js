4c492d4a64945277dcfc3e4fc0d3976bc842c91f
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-06-11 14:42:29 +0000

    build based on 939c154

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#KernelFunctions.jl-1","page":"KernelFunctions.jl","title":"KernelFunctions.jl","text":"","category":"section"},{"location":"#","page":"KernelFunctions.jl","title":"KernelFunctions.jl","text":"Documentation for KernelFunctions.jl","category":"page"},{"location":"#","page":"KernelFunctions.jl","title":"KernelFunctions.jl","text":"*** In Construction ***","category":"page"}]
+}
