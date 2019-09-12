35e4a1f27b4c36d457e21a00e04c4e654bbd8234
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2019-09-12 15:48:05 +0200

    Renamed sqexponential.jl in exponential.jl

--- src/KernelFunctions.jl
+++ src/KernelFunctions.jl
@@ -1,7 +1,10 @@
 module KernelFunctions
 
 export kernelmatrix, kernelmatrix!, kerneldiagmatrix, kerneldiagmatrix!, kappa
-export Kernel, SqExponentialKernel, ExponentialKernel, MaternKernel, Matern32Kernel, Matern52Kernel
+export Kernel
+export SqExponentialKernel, ExponentialKernel, GammaExponentialKernel
+export MaternKernel, Matern32Kernel, Matern52Kernel
+export LinearKernel, PolynomialKernel
 
 export Transform, ScaleTransform
 
@@ -16,10 +19,11 @@ abstract type Kernel{T,Tr} end
 include("zygote_rules.jl")
 include("utils.jl")
 include("distances/dotproduct.jl")
+include("distances/delta.jl")
 include("transform/transform.jl")
 include("kernelmatrix.jl")
 
-kernels = ["sqexponential","matern","polynomial","constant"]
+kernels = ["exponential","matern","polynomial","constant"]
 for k in kernels
     include(joinpath("kernels",k*".jl"))
 end
---
+++ src/kernels/exponential.jl
@@ -0,0 +1,114 @@
+"""
+    SqExponentialKernel([ρ=1.0])
+
+The squared exponential kernel is an isotropic Mercer kernel given by the formula:
+
+```
+    κ(x,y) = exp(-‖x-y‖²)
+```
+
+See also [`ExponentialKernel`](@ref) for a
+related form of the kernel or [`GammaExponentialKernel`](@ref) for a generalization.
+
+# Examples
+
+```jldoctest; setup = :(using KernelFunctions)
+julia> SqExponentialKernel()
+SqExponentialKernel{Float64,Float64}(1.0)
+
+julia> SqExponentialKernel(2.0f0)
+SqExponentialKernel{Float32,Float32}(2.0)
+
+julia> SqExponentialKernel([2.0,3.0])
+SqExponentialKernel{Float64,Array{Float64}}([2.0,3.0])
+```
+"""
+struct SqExponentialKernel{T,Tr<:Transform} <: Kernel{T,Tr}
+    transform::Tr
+    metric::SqEuclidean
+    function SqExponentialKernel{T,Tr}(transform::Tr) where {T,Tr<:Transform}
+        return new{T,Tr}(transform,SqEuclidean())
+    end
+end
+
+@inline kappa(κ::SqExponentialKernel, d²::Real) where {T} = exp(-d²)
+
+"""
+    ExponentialKernel([ρ=1.0])
+
+The exponential kernel is an isotropic Mercer kernel given by the formula:
+
+```
+    κ(x,y) = exp(-‖x-y‖)
+```
+
+# Examples
+
+```jldoctest; setup = :(using KernelFunctions)
+julia> ExponentialKernel()
+ExponentialKernel{Float64,Float64}(1.0)
+
+julia> ExponentialKernel(2.0f0)
+ExponentialKernel{Float32,Float32}(2.0)
+
+julia> ExponentialKernel([2.0,3.0])
+ExponentialKernel{Float64,Array{Float64}}([2.0,3.0])
+```
+"""
+struct ExponentialKernel{T,Tr<:Transform} <: Kernel{T,Tr}
+    transform::Tr
+    metric::Euclidean
+    function ExponentialKernel{T,Tr}(transform::Tr) where {T,Tr<:Transform}
+        return new{T,Tr}(transform,Euclidean())
+    end
+end
+
+@inline kappa(κ::ExponentialKernel, d::Real) where {T} = exp(-d)
+
+"""
+    GammaExponentialKernel([ρ=1.0,[gamma=2.0]])
+
+The γ-exponential kernel is an isotropic Mercer kernel given by the formula:
+
+```
+    κ(x,y) = exp(-‖x-y‖^2γ)
+```
+
+# Examples
+
+```jldoctest; setup = :(using KernelFunctions)
+julia> GammaExponentialKernel()
+GammaExponentialKernel{Float64,Float64,Float64}(1.0,2.0)
+
+julia> GammaExponentialKernel(2.0f0,3.0)
+GammaExponentialKernel{Float32,Float32,Float64}(2.0,3.0)
+
+julia> GammaExponentialKernel([2.0,3.0],2f0)
+GammaExponentialKernel{Float64,Array{Float64},Float32}([2.0,3.0],2.0)
+```
+"""
+struct GammaExponentialKernel{T,Tr<:Transform,Tᵧ<:Real} <: Kernel{T,Tr,Tᵧ}
+    transform::Tr
+    metric::SqEuclidean
+    γ::Tᵧ
+    function GammaExponentialKernel{T,Tr,Tᵧ}(transform::Tr,γ::Tᵧ) where {T,Tr<:Transform,Tᵧ<:Real}
+        return new{T,Tr,Tᵧ}(transform,SqEuclidean(),γ)
+    end
+end
+
+function GammaExponentialKernel(ρ::T₁=1.0,gamma::T₂=2.0) where {T₁<:Real,T₂<:Real}
+    @check_args(GammaExponentialKernel, gamma, gamma >= zero(T₂), "gamma > 0")
+    Polynomial{T₁,ScaleTransform{T₁},T₂}(ScaleTransform(ρ),gamma)
+end
+
+function GammaExponentialKernel(ρ::A,gamma::T₁=2.0) where {A<:AbstractVector{<:Real},T₁<:Real}
+    @check_args(GammaExponentialKernel, gamma, gamma >= zero(T₁), "gamma > 0")
+    GammaExponentialKernel{eltype(A),ScaleTransform{A},T₁}(ScaleTransform(ρ),gamma)
+end
+
+function GammaExponentialKernel(t::Tr,gamma::T₁=2.0) where {Tr<:Transform,T₁<:Real}
+    @check_args(GammaExponentialKernel, gamma, gamma >= zero(T₁), "gamma > 0")
+    GammaExponentialKernel{eltype(Tr),Tr,T₁}(t,gamma)
+end
+
+@inline kappa(κ::GammaExponentialKernel, d²::Real) where {T} = exp(-d²^γ)
