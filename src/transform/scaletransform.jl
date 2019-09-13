089dec7e9101a1953447c94e707fd56640d2f20a
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2019-09-13 18:00:10 +0200

    Transfered scale transform to new file

---
+++ src/transform/scaletransform.jl
@@ -0,0 +1,33 @@
+struct ScaleTransform{T<:Union{Real,AbstractVector{<:Real}}} <: Transform
+    s::T
+end
+
+function ScaleTransform(s::T=1.0) where {T<:Real}
+    @check_args(ScaleTransform, s, s > zero(T), "s > 0")
+    ScaleTransform{T}(s)
+end
+
+function ScaleTransform(s::T,dims::Integer) where {T<:Real}
+    @check_args(ScaleTransform, s, s > zero(T), "s > 0")
+    ScaleTransform{Vector{T}}(fill(s,dims))
+end
+
+function ScaleTransform(s::A) where {A<:AbstractVector{<:Real}}
+    @check_args(ScaleTransform, s, all(s.>zero(eltype(A))), "s > 0")
+    ScaleTransform{A}(s)
+end
+
+dim(str::ScaleTransform{<:Real}) = 1
+dim(str::ScaleTransform{<:AbstractVector{<:Real}}) = length(str.s)
+
+function transform(t::ScaleTransform{<:AbstractVector{<:Real}},X::AbstractMatrix{<:Real},obsdim::Int)
+    @boundscheck if dim(t) != size(X,!Bool(obsdim-1)+1)
+        throw(DimensionMismatch("Array has size $(size(X,!Bool(obsdim-1)+1)) on dimension $(!Bool(obsdim-1)+1)) which does not match the length of the scale transform length , $(dim(t))."))
+    end
+    _transform(t,X,obsdim)
+end
+_transform(t::ScaleTransform{<:AbstractVector{<:Real}},x::AbstractVector{<:Real}) = t.s .* x
+_transform(t::ScaleTransform{<:AbstractVector{<:Real}},X::AbstractMatrix{<:Real},obsdim::Int) = obsdim == 1 ? t.s'.*X : t.s .* X
+
+transform(t::ScaleTransform{<:Real},x::AbstractVecOrMat,obsdim::Int) = transform(t,x)
+transform(t::ScaleTransform{<:Real},x::AbstractVecOrMat) = t.s .* x
