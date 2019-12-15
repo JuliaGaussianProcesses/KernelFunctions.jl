b0acc6f999307f2a84947f388c2d84567a5b758b
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2019-12-15 21:13:19 +0100

    Corrected most tests

--- test/utils_AD.jl
+++ test/utils_AD.jl
@@ -1,4 +1,6 @@
 allapprox(x,y,tol=1e-8) = all(isapprox.(x,y,atol=tol))
+FDM = central_fdm(5,1)
+
 
 function kappa_AD(::Val{:Zygote},k::Kernel,d::Real)
     first(Zygote.gradient(x->kappa(k,x),d))
@@ -9,29 +11,29 @@ function kappa_AD(::Val{:ForwardDiff},k::Kernel,d::Real)
 end
 
 function kappa_fdm(k::Kernel,d::Real)
-    central_fdm(5,1)(x->kappa(k,x),d)
+    first(FiniteDifferences.grad(FDM,x->kappa(k,x),d))
 end
 
 
 function transform_AD(::Val{:Zygote},t::Transform,A)
     ps = KernelFunctions.params(t)
-    @test allisapprox(first(Zygote.gradient(p->transform_with_duplicate(p,t,A),ps)),
-            central_fdm(5,1)(p->transform_with_duplicate(p,t,A),ps))
-    @test allisapprox(first(Zygote.gradient(X->sum(transform(t,X,2)),A))
-            .≈ central_fdm(5,1)(X->sum(transform(t,X,2)),A))
+    @test allapprox(first(Zygote.gradient(p->transform_with_duplicate(p,t,A),ps)),
+        first(FiniteDifferences.grad(FDM,p->transform_with_duplicate(p,t,A),ps)))
+    @test allapprox(first(Zygote.gradient(X->sum(transform(t,X,2)),A)),
+            first(FiniteDifferences.grad(FDM,X->sum(transform(t,X,2)),A)))
 end
 
 function transform_AD(::Val{:ForwardDiff},t::Transform,A)
     ps = KernelFunctions.params(t)
     if t isa ScaleTransform
-        @test allisapprox(first(ForwardDiff.gradient(p->transform_with_duplicate(first(p),t,A),[ps])),
-            central_fdm(5,1)(p->transform_with_duplicate(p,t,A),ps))
+        @test allapprox(first(ForwardDiff.gradient(p->transform_with_duplicate(first(p),t,A),[ps])),
+            first(FiniteDifferences.grad(FDM,p->transform_with_duplicate(p,t,A),ps)))
     else
-        @test allisapprox(ForwardDiff.gradient(p->transform_with_duplicate(p,t,A),ps),
-            central_fdm(5,1)(p->transform_with_duplicate(p,t,A),ps))
+        @test allapprox(ForwardDiff.gradient(p->transform_with_duplicate(p,t,A),ps),
+            first(FiniteDifferences.grad(FDM,p->transform_with_duplicate(p,t,A),ps)))
     end
-    @test allisapprox(ForwardDiff.gradient(X->sum(transform(t,X,2)),A),
-            central_fdm(5,1)(X->sum(transform(t,X,2)),A))
+    @test allapprox(ForwardDiff.gradient(X->sum(transform(t,X,2)),A),
+            first(FiniteDifferences.grad(FDM,X->sum(transform(t,X,2)),A)))
 end
 
 transform_with_duplicate(p,t,A) = sum(transform(KernelFunctions.duplicate(t,p),A,2))
