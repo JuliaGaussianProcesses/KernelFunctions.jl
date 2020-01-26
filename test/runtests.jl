0b9fdbfdc4fc12e135a589372e02ff5840487b32
Isak Falk <me@isakfalk.com>
2020-01-26 15:25:05 +0000

    Add tests for nystrom.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using Random
 
 @testset "KernelFunctions" begin
 include("test_kernelmatrix.jl")
+include("test_approximations.jl")
 include("test_constructors.jl")
 # include("test_AD.jl")
 include("test_transform.jl")
