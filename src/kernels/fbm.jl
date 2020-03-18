63c715c4f06c8c4d54498131b569f3b11befd8bf
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-03-19 01:27:46 +0530

    Cover more cases

--- src/kernels/fbm.jl
+++ src/kernels/fbm.jl
@@ -1,8 +1,8 @@
 """
-    FBMKernel
+    FBMKernel(; h::Real=0.5)
 Fractional Brownian motion kernel with Hurst index h from (0,1) given by
 ```
-    κ(x,y) =  ( |x|²ʰ + |z|²ʰ - |x-z|²ʰ ) / 2
+    κ(x,y) =  ( |x|²ʰ + |y|²ʰ - |x-y|²ʰ ) / 2
 ```
 
 For h=1/2, this is the Wiener Kernel, for h>1/2, the increments are
@@ -19,7 +19,7 @@ end
 
 kappa(κ::FBMKernel, d::Real) = error("Not Implemented: Please use `kernelmatrix` or `kerneldiagmatrix` instead.")
 
-_fbm(modX, modY, modXY, h) = (modX^h + modY^h + modXY^h)/2
+_fbm(modX, modY, modXY, h) = (modX^h + modY^h - modXY^h)/2
 
 function kernelmatrix(κ::FBMKernel, X::AbstractMatrix; obsdim::Int = defaultobs)
     @assert obsdim ∈ [1,2] "obsdim should be 1 or 2 (see docs of kernelmatrix))"
@@ -37,6 +37,16 @@ function kernelmatrix(
     modX = pairwise(SqEuclidean(),X,dims=obsdim)
     modY = pairwise(SqEuclidean(),Y,dims=obsdim)
     modXY = pairwise(SqEuclidean(),X-Y,dims=obsdim)
-    @info modXY
     K = map((x)->_fbm(x[1], x[2], x[3], κ.h), zip(modX, modY, modXY))
 end
+
+#Syntactic Sugar
+function (κ::FBMKernel)(x::AbstractVector{<:Real}, y::AbstractVector{<:Real})
+    modX = evaluate(SqEuclidean(), x, x)
+    modY = evaluate(SqEuclidean(), y, y)
+    modXY = evaluate(SqEuclidean(), x-y, x-y)
+    (modX^κ.h + modY^κ.h - modXY^κ.h)/2
+end
+
+(κ::FBMKernel)(X::AbstractMatrix{T}, Y::AbstractMatrix{T}; obsdim::Integer=defaultobs) where {T} = kernelmatrix(κ, X, Y, obsdim=obsdim)
+(κ::FBMKernel)(X::AbstractMatrix{T}; obsdim::Integer=defaultobs) where {T} = kernelmatrix(κ, X, obsdim=obsdim)
