0eba7761a532e537b55fa26c58ed54e6e756d6c4
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-03-19 00:48:36 +0530

    Add kernalmatrix function(s)

--- src/kernels/fbm.jl
+++ src/kernels/fbm.jl
@@ -12,11 +12,31 @@ positively correlated and for h<1/2 the increments are negatively correlated.
 struct FBMKernel{T<:Real} <: BaseKernel
     h::T
     function FBMKernel(;h::T=0.5) where {T<:Real}
-        @assert h<=1.0 && h>=0.0, "FBMKernel: Given Hurst index h is invalid."
+        @assert h<=1.0 && h>=0.0 "FBMKernel: Given Hurst index h is invalid."
         new{T}(h)
     end
 end
 
-kappa(κ::FBMKernel, d::Real) = 0 # to be implemented
+kappa(κ::FBMKernel, d::Real) = error("Not Implemented: Please use `kernelmatrix` or `kerneldiagmatrix` instead.")
 
-metric(::FBMKernel) = Euclidean()
+_fbm(modX, modY, modXY, h) = (modX^h + modY^h + modXY^h)/2
+
+function kernelmatrix(κ::FBMKernel, X::AbstractMatrix; obsdim::Int = defaultobs)
+    @assert obsdim ∈ [1,2] "obsdim should be 1 or 2 (see docs of kernelmatrix))"
+    K = map(modX->_fbm(modX, modX, 0, κ.h), pairwise(SqEuclidean(),X,dims=obsdim))
+end
+
+function kernelmatrix(
+    κ::FBMKernel,
+    X::AbstractMatrix,
+    Y::AbstractMatrix;
+    obsdim::Int = defaultobs,
+)
+    @assert obsdim ∈ [1,2] "obsdim should be 1 or 2 (see docs of kernelmatrix))"
+    
+    modX = pairwise(SqEuclidean(),X,dims=obsdim)
+    modY = pairwise(SqEuclidean(),Y,dims=obsdim)
+    modXY = pairwise(SqEuclidean(),X-Y,dims=obsdim)
+    @info modXY
+    K = map((x)->_fbm(x[1], x[2], x[3], κ.h), zip(modX, modY, modXY))
+end
