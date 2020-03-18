cdccace2babd6dc5288963a1e53ae0b69cfb8c52
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-03-18 11:45:36 +0100

    Used SemiMetric and added eval_op for later use

--- src/distances/sinus.jl
+++ src/distances/sinus.jl
@@ -1,13 +1,18 @@
-struct Sinus{T} <: Distances.PreMetric
+struct Sinus{T} <: Distances.SemiMetric
     r::Vector{T}
 end
 
-@inline function Distances._evaluate(d::Sinus,a::AbstractVector{T},b::AbstractVector{T}) where {T}
+Distances.parameters(d::Sinus) = d.r
+
+@inline function Distances._evaluate(d::Sinus, a::AbstractVector{T}, b::AbstractVector{T}) where {T}
     @boundscheck if (length(a) != length(b)) || length(a) != length(d.r)
         throw(DimensionMismatch("Dimensions of the inputs are not matching : a = $(length(a)), b = $(length(b)), r = $(length(d.r))"))
     end
     return sum(abs2,sin.(π.*(a-b))./d.r)
 end
 
-@inline (dist::Sinus)(a::AbstractArray,b::AbstractArray) = Distances._evaluate(dist,a,b)
-@inline (dist::Sinus)(a::Number,b::Number) = (sin(π*(a-b))/first(dist.r))^2
+# For later convenience once Distances.jl open their API
+@inline Distances.eval_op(::Sinus, a::Real, b::Real, p::Real) = abs2(sin(π * (a - b)) / p)
+
+@inline (dist::Sinus)(a::AbstractArray,b::AbstractArray) = Distances._evaluate(dist, a, b)
+@inline (dist::Sinus)(a::Number,b::Number) = abs2(sin(π * (a - b)) / first(dist.r))
