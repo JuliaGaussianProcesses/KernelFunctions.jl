836ccb66eeee69d3b7fd732602db749acb938ec2
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-03-21 00:09:00 +0530

    Add GaborKernel

---
+++ src/kernels/gabor.jl
@@ -0,0 +1,20 @@
+"""
+    GaborKernel(; ell::Real=1.0, p::Real=1.0)
+
+Gabor kernel with length scale ell and period p. Given by
+```
+    κ(x,y) =  h(x-z), h(t) = exp(-sum(t.^2./(2*ell.^2)))*cos(2*pi*sum(t./p))
+```
+
+"""
+struct GaborKernel{T<:Real} <: BaseKernel
+    ell::T
+    p::T
+    function GaborKernel(;ell::T=1.0, p::T=1.0) where {T<:Real}
+        new{T}(ell, p)
+    end
+end
+
+kappa(κ::GaborKernel, d::T) where {T<:Real} = exp(-sum(d.^2 ./(2*κ.ell.^2)))*cospi(2*sum(d)./ κ.p)
+
+metric(::GaborKernel) = Euclidean()
