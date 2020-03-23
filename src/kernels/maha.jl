b1bd1d31dc15e405562de09c8d656e6d5c3725bc
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-03-24 00:25:09 +0530

    Modify Mahalanobis distance-based kernel

--- src/kernels/maha.jl
+++ src/kernels/maha.jl
@@ -1,21 +1,22 @@
 """
-    MahaKernel(; ell::Real=1.0, p::Real=1.0)
+    MahaKernel(P::AbstractMatrix, kappa::Function)
 
     Mahalanobis distance-based kernel given by
 ```math
-    κ(x,y) =  k(r^2), r^2 = maha(x,P,y) = (x-y)'*inv(P)*(x-y)
+    κ(x,y) =  kappa(r^2), r^2 = maha(x,P,y) = (x-y)'*inv(P)*(x-y)
 ```
-where the matrix P is the metric.
+where the matrix P is the metric and kappa is a user defined kernel function.
 
 """
 struct MahaKernel{T<:Real} <: BaseKernel
-    P
-    function MahaKernel(P::AbstractMatrix{T}) where {T<:Real}
-        @assert size(P)[1] == size(P)[2], "P should be a square matrix"
-        new{T}(P)
+    P::AbstractMatrix{T}
+    kappa::Function
+    function MahaKernel(P::AbstractMatrix{T}, kappa::Function) where {T<:Real}
+        @assert size(P)[1] == size(P)[2] "P should be a square matrix"
+        new{T}(P, kappa)
     end
 end
 
-kappa(κ::MahaKernel, d::T) where {T<:Real} = d 
+kappa(κ::MahaKernel, d::T) where {T<:Real} = κ.kappa(d) 
 
 metric(κ::MahaKernel) = Mahalanobis(κ.P)
