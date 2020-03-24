4109fe853b2f75358f2169acb65056d4dc2d6bb0
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-03-24 16:01:41 +0530

    Use exponentiation as kappa function

--- src/kernels/maha.jl
+++ src/kernels/maha.jl
@@ -1,22 +1,21 @@
 """
-    MahaKernel(P::AbstractMatrix, kappa::Function)
+    MahalanobisKernel(P::AbstractMatrix, kappa::Function)
 
     Mahalanobis distance-based kernel given by
 ```math
-    κ(x,y) =  kappa(r^2), r^2 = maha(x,P,y) = (x-y)'*inv(P)*(x-y)
+    κ(x,y) =  exp(-r^2), r^2 = maha(x,P,y) = (x-y)'*inv(P)*(x-y)
 ```
-where the matrix P is the metric and kappa is a user defined kernel function.
+where the matrix P is the metric.
 
 """
-struct MahaKernel{T<:Real} <: BaseKernel
-    P::AbstractMatrix{T}
-    kappa::Function
-    function MahaKernel(P::AbstractMatrix{T}, kappa::Function) where {T<:Real}
+struct MahalanobisKernel{T<:Real,A<:AbstractMatrix{T}} <: BaseKernel
+    P::A
+    function MahalanobisKernel(P::AbstractMatrix{T}) where {T<:Real}
         LinearAlgebra.checksquare(P)
-        new{T}(P, kappa)
+        new{T}(P)
     end
 end
 
-kappa(κ::MahaKernel, d::T) where {T<:Real} = κ.kappa(d) 
+kappa(κ::MahalanobisKernel, d::T) where {T<:Real} = exp(-d) 
 
-metric(κ::MahaKernel) = SqMahalanobis(κ.P)
+metric(κ::MahalanobisKernel) = SqMahalanobis(κ.P)
