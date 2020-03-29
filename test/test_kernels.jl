8b821db95e832c5f74425bbf877baca7fe70f42e
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-03-29 20:29:14 +0530

    Add more tests

--- test/test_kernels.jl
+++ test/test_kernels.jl
@@ -91,6 +91,10 @@ x = rand()*2; v1 = rand(3); v2 = rand(3); id = IdentityTransform()
         @test k.p ≈ p atol=1e-5
         @test kappa(k,v1,v2) ≈ exp(-sqeuclidean(v1,v2) ./(k.ell.^2))*cospi(euclidean(v1,v2)./ k.p) atol=1e-5
         @test kappa(k,v1,v2) ≈ kappa(transform(SqExponentialKernel(), 1/k.ell),v1,v2)*kappa(transform(CosineKernel(), 1/k.p), v1,v2) atol=1e-5
+
+        k = GaborKernel()
+        @test k.ell ≈ 1.0 atol=1e-5
+        @test k.p ≈ 1.0 atol=1e-5
     end
     @testset "Matern" begin
         @testset "MaternKernel" begin
