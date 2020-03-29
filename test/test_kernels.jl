40b25dbaad9f7e44d86fd4520c64097ecbddb015
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-03-29 19:01:01 +0530

    Make tests pass

--- test/test_kernels.jl
+++ test/test_kernels.jl
@@ -87,8 +87,8 @@ x = rand()*2; v1 = rand(3); v2 = rand(3); id = IdentityTransform()
         ell = abs(rand())
         p = abs(rand())
         k = GaborKernel(ell=ell, p=p)
-        @test k.ell == ell
-        @test k.p == p
+        @test k.ell ≈ ell atol=1e-5
+        @test k.p ≈ p atol=1e-5
         @test kappa(k,v1,v2) ≈ exp(-sqeuclidean(v1,v2) ./(k.ell.^2))*cospi(euclidean(v1,v2)./ k.p) atol=1e-5
         @test kappa(k,v1,v2) ≈ kappa(transform(SqExponentialKernel(), 1/k.ell),v1,v2)*kappa(transform(CosineKernel(), 1/k.p), v1,v2) atol=1e-5
     end
