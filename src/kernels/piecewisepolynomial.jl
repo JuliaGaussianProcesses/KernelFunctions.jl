ee263faedcb2e2008172014c8da8d33d3b71bff0
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-04-08 19:42:20 +0530

    Apply suggestions from code review
    Co-Authored-By: willtebbutt <wt0881@my.bristol.ac.uk>

--- src/kernels/piecewisepolynomial.jl
+++ src/kernels/piecewisepolynomial.jl
@@ -15,7 +15,7 @@ struct PiecewisePolynomialKernel{V, A<:AbstractMatrix{<:Real}} <: BaseKernel
     function PiecewisePolynomialKernel{V}(maha::AbstractMatrix{<:Real}) where V
         V in (0, 1, 2, 3) || error("Invalid paramter v=$(V). Should be 0, 1, 2 or 3.")
         LinearAlgebra.checksquare(maha)
-        new{V,typeof(maha)}(maha)
+        return new{V,typeof(maha)}(maha)
     end
 end
 
@@ -27,21 +27,26 @@ function _f(κ::PiecewisePolynomialKernel{V}, r, j) where V
     if V==0
         return 1
     elseif V==1
-        return 1 + (j+1)*r
+        return 1 + (j + 1) * r
     elseif V==2
-        return 1 + (j+2)*r +   (j^2+ 4*j+ 3)/ 3*r.^2
+        return 1 + (j + 2) * r + (j^2 + 4 * j + 3) / 3 * r.^2
     elseif V==3
-        return 1 + (j+3)*r +   (6*j^2+36*j+45)/15*r.^2 + (j^3 + 9*j^2 + 23*j + 15)/15 * r.^3
+        return 1 + (j + 3) * r + (6 * j^2 + 36j + 45) / 15 * r.^2 +
+            (j^3 + 9 * j^2 + 23j + 15) / 15 * r.^3
     else
         error("Invalid paramter v=$(V). Should be 0,1,2 or 3.")
     end
 end
 
 function _piecewisepolynomial(κ::PiecewisePolynomialKernel{V}, r, j) where V
-    return max(1-r,0)^(j + V) * _f(κ,r,j)
+    return max(1 - r, 0)^(j + V) * _f(κ, r, j)
 end
 
-function kappa(κ::PiecewisePolynomialKernel{V}, x::AbstractVector{<:Real}, y::AbstractVector{<:Real}) where {V}
+function kappa(
+    κ::PiecewisePolynomialKernel{V},
+    x::AbstractVector{<:Real},
+    y::AbstractVector{<:Real},
+) where {V}
     r = evaluate(metric(κ), x, y)
     j = div(size(x, 2), 1) + V + 1
     return _piecewisepolynomial(κ, r, j)
@@ -51,10 +56,10 @@ function _kernel(
     κ::PiecewisePolynomialKernel,
     x::AbstractVector,
     y::AbstractVector;
-    obsdim::Int = defaultobs
+    obsdim::Int = defaultobs,
 )
     @assert length(x) == length(y) "x and y don't have the same dimension!"
-    kappa(κ,x,y)
+    return kappa(κ,x,y)
 end
 
 function kernelmatrix(
@@ -63,12 +68,12 @@ function kernelmatrix(
     obsdim::Int = defaultobs
 ) where {V}
     j = div(size(X, feature_dim(obsdim)), 2) + V + 1
-    return map(r->_piecewisepolynomial(κ,r,j),pairwise(metric(κ),X,dims=obsdim))
+    return map(r->_piecewisepolynomial(κ, r, j), pairwise(metric(κ), X; dims=obsdim))
 end
 
-function _kernelmatrix(κ::PiecewisePolynomialKernel{V},X,Y,obsdim) where {V}
+function _kernelmatrix(κ::PiecewisePolynomialKernel{V}, X, Y; obsdim) where {V}
     j = div(size(X, feature_dim(obsdim)), 2) + V + 1
-    return map(r->_piecewisepolynomial(κ,r,j),pairwise(metric(κ),X,Y,dims=obsdim))
+    return map(r->_piecewisepolynomial(κ, r, j), pairwise(metric(κ), X, Y; dims=obsdim))
 end
 
 function kernelmatrix!(
@@ -78,11 +83,14 @@ function kernelmatrix!(
     obsdim::Int = defaultobs
 ) where {V}
     @assert obsdim ∈ [1,2] "obsdim should be 1 or 2 (see docs of kernelmatrix))"
-    if !check_dims(K,X,X,feature_dim(obsdim),obsdim)
-        throw(DimensionMismatch("Dimensions of the target array K $(size(K)) are not consistent with X $(size(X))"))
+    if !check_dims(K, X, X, feature_dim(obsdim), obsdim)
+        throw(DimensionMismatch(
+            "Dimensions of the target array K $(size(K)) are not consistent with X " *
+            "$(size(X))",
+        ))
     end
     j = div(size(X, feature_dim(obsdim)), 2) + V + 1
-    map!(r->_piecewisepolynomial(κ,r,j),K,pairwise(metric(κ),X,dims=obsdim))
+    return map!(r->_piecewisepolynomial(κ,r,j), K, pairwise(metric(κ), X; dims=obsdim))
 end
 
 function kernelmatrix!(
@@ -90,16 +98,21 @@ function kernelmatrix!(
     κ::PiecewisePolynomialKernel{V},
     X::AbstractMatrix,
     Y::AbstractMatrix;
-    obsdim::Int = defaultobs
+    obsdim::Int = defaultobs,
 ) where {V}
     @assert obsdim ∈ [1,2] "obsdim should be 1 or 2 (see docs of kernelmatrix))"
-    if !check_dims(K,X,Y,feature_dim(obsdim),obsdim)
-        throw(DimensionMismatch("Dimensions $(size(K)) of the target array K are not consistent with X ($(size(X))) and Y ($(size(Y)))"))
+    if !check_dims(K, X, Y, feature_dim(obsdim), obsdim)
+        throw(DimensionMismatch(
+            "Dimensions $(size(K)) of the target array K are not consistent with X " *
+            "($(size(X))) and Y ($(size(Y)))",
+        ))
     end
     j = div(size(X, feature_dim(obsdim)), 2) + V + 1
-    map!(r->_piecewisepolynomial(κ,r,j),K,pairwise(metric(κ),X,Y,dims=obsdim))
+    return map!(r->_piecewisepolynomial(κ,r,j), K, pairwise(metric(κ), X, Y; dims=obsdim))
 end
 
 metric(κ::PiecewisePolynomialKernel) = Mahalanobis(κ.maha)
 
-Base.show(io::IO, κ::PiecewisePolynomialKernel{V}) where V = print(io, "Piecewise Polynomial Kernel (v = $(V), size(maha) = $(size(κ.maha))")
+function Base.show(io::IO, κ::PiecewisePolynomialKernel{V}) where {V}
+    print(io, "Piecewise Polynomial Kernel (v = $(V), size(maha) = $(size(κ.maha))")
+end
