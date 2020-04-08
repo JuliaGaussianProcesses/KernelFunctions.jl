94db1c95d5f4d9a922ec59f176b609a1f640d9b9
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-04-08 19:48:02 +0530

    Fix doctring

--- src/kernels/piecewisepolynomial.jl
+++ src/kernels/piecewisepolynomial.jl
@@ -5,7 +5,7 @@ Piecewise Polynomial covariance function with compact support, V = 0,1,2,3.
 The kernel functions are 2v times continuously differentiable and the corresponding
 processes are hence v times  mean-square differentiable. The kernel function is:
 ```math
-    κ(x,y) = max(1-r,0)^(j+V) * f(r,j) with j = floor(D/2)+V+1
+    κ(x, y) = max(1 - r, 0)^(j + V) * f(r, j) with j = floor(D / 2) + V + 1
 ```
 where `r` is the Mahalanobis distance mahalanobis(x,y) with `maha` as the metric.
 
