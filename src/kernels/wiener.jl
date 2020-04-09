5722672eb0eda164ac0d45d2d64b95efc6a4f46b
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-04-09 17:43:46 +0530

    Add kernel matrix functions

--- src/kernels/wiener.jl
+++ src/kernels/wiener.jl
@@ -71,13 +71,86 @@ function _kernel(
     kappa(κ,x,y)
 end
 
+function kernelmatrix!(
+    K::AbstractMatrix,
+    κ::WienerKernel,
+    X::AbstractMatrix,
+    Y::AbstractMatrix;
+    obsdim::Int = defaultobs
+)
+    @assert obsdim ∈ [1,2] "obsdim should be 1 or 2 (see docs of kernelmatrix))"
+    if !check_dims(K,X,X,feature_dim(obsdim),obsdim)
+        throw(DimensionMismatch("Dimensions of the target array K $(size(K)) are not consistent with X $(size(X))"))
+    end
+
+    if obsdim == 1
+        for j = 1:size(K,2)
+            for i = 1:size(K,1)
+                @inbounds @views K[i,j] = _kernel(κ, X[i,:],Y[j,:])
+            end
+        end
+    else
+        for j = 1:size(K,2)
+            for i = 1:size(K,1)
+                @inbounds @views K[i,j] = _kernel(κ, X[:,i],Y[:,j])
+            end
+        end
+    end
+    return K
+end
+
+function kernelmatrix(
+    κ::WienerKernel,
+    X::AbstractMatrix,
+    Y::AbstractMatrix;
+    obsdim::Int = defaultobs
+)
+    @assert obsdim ∈ [1,2] "obsdim should be 1 or 2 (see docs of kernelmatrix))"
+    if !check_dims(X,Y,feature_dim(obsdim),obsdim)
+        throw(DimensionMismatch("X $(size(X)) and Y $(size(Y)) do not have the same number of features on the dimension : $(feature_dim(obsdim))"))
+    end
+    if obsdim == 1
+        outdim = size(X,1)
+    else
+        outdim = size(X,2)
+    end
+    K = zeros(outdim,outdim)
+    if obsdim == 1
+        for j = 1:size(K,2)
+            for i = 1:size(K,1)
+                @inbounds @views K[i,j] = _kernel(κ, X[i,:],Y[j,:])
+            end
+        end
+    else
+        for j = 1:size(K,2)
+            for i = 1:size(K,1)
+                @inbounds @views K[i,j] = _kernel(κ, X[:,i],Y[:,j])
+            end
+        end
+    end
+    return K
+end
+
+function kernelmatrix!(
+    K::AbstractMatrix,
+    κ::WienerKernel,
+    X::AbstractMatrix;
+    obsdim::Int = defaultobs
+)
+    @assert obsdim ∈ [1,2] "obsdim should be 1 or 2 (see docs of kernelmatrix))"
+    if !check_dims(K,X,X,feature_dim(obsdim),obsdim)
+        throw(DimensionMismatch("Dimensions of the target array K $(size(K)) are not consistent with X $(size(X))"))
+    end
+    kernelmatrix!(K,κ,X,X;obsdim=obsdim)
+    return K
+end
+
 function kernelmatrix(
     κ::WienerKernel,
     X::AbstractMatrix;
     obsdim::Int = defaultobs
 )
-    return map(r->_piecewisepolynomial(κ,r,j),pairwise(metric(κ),X,dims=obsdim))
+    return kernelmatrix(κ,X,X;obsdim=obsdim)
 end
-
 
 Base.show(io::IO, κ::WienerKernel{I}) where I = print(io, "Wiener Kernel $(I)-times integrated")
