b2881579e8a01b1caa957251816be23b3d9a817d
David Widmann <dev+git@devmotion.de>
2020-04-16 16:08:08 +0200

    Fix Mahalanobis test

--- test/basekernels/maha.jl
+++ test/basekernels/maha.jl
@@ -1,7 +1,13 @@
 @testset "maha" begin
-    P = rand(3,3)
+    rng = MersenneTwister(123456)
+    x = 2 * rand(rng)
+    v1 = rand(rng, 3)
+    v2 = rand(rng, 3)
+
+    P = rand(rng, 3, 3)
     k = MahalanobisKernel(P)
-    @test kappa(k,x) == exp(-x)
-    @test k(v1,v2) ≈ exp(-sqmahalanobis(v1,v2, k.P))
-    @test kappa(ExponentialKernel(),x) == kappa(k,x)
+
+    @test kappa(k, x) == exp(-x)
+    @test k(v1, v2) ≈ exp(-sqmahalanobis(v1, v2, P))
+    @test kappa(ExponentialKernel(), x) == kappa(k, x)
 end
