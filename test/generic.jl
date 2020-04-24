3369cdeba38557de57c28bf114958e5d0e79d1c3
willtebbutt <wt0881@my.bristol.ac.uk>
2020-04-24 21:00:30 +0100

    Adds pairwise for AV{<:Real}

--- test/generic.jl
+++ test/generic.jl
@@ -3,4 +3,12 @@
     @test length(k) == 1
     @test iterate(k) == (k,nothing)
     @test iterate(k,1) == nothing
+
+    rng = MersenneTwister(123456)
+    x = randn(rng, 10)
+    X = reshape(x, :, 1)
+    y = randn(rng, 11)
+    Y = reshape(y, :, 1)
+    @test pairwise(SqEuclidean(), x, y) ≈ pairwise(SqEuclidean(), X, Y; dims=1)
+    @test pairwise(SqEuclidean(), x) ≈ pairwise(SqEuclidean(), X; dims=1)
 end
