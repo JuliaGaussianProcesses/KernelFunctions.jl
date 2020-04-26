f10c94e1eb65a13f48e5580c30e8e0db60f11ba0
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-04-26 18:16:43 +0530

    Update docstring

--- src/basekernels/nn.jl
+++ src/basekernels/nn.jl
@@ -3,9 +3,16 @@
 
 Neural network kernel function.
 
-```julia
+```math
     κ(x, y) =  asin(x' * y / sqrt[(1 + x' * x) * (1 + y' * y)])
 ```
+# Significance:
+Neal(1996)  pursued the limits of large models, and showed that a Bayesian neural network becomes a Gaussian process with a **neural network kernel** as the number of units approaches infinity.
+
+# References:
+- Gaussian Processes for Machine Learning Pg 105 - http://www.gaussianprocess.org/gpml/chapters/RW4.pdf
+- Neal(1996) - https://www.cs.toronto.edu/~radford/bnn.book.html
+- Andrew Gordon's Thesis Pg 45 - http://www.cs.cmu.edu/~andrewgw/andrewgwthesis.pdf
 
 """
 struct NeuralNetworkKernel <: BaseKernel end
