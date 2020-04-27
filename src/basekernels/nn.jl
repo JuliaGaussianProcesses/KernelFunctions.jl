828a306f2cb18fe1e33c4a28bf86f6510bc31dc8
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-04-27 20:49:06 +0530

    Update docs

--- src/basekernels/nn.jl
+++ src/basekernels/nn.jl
@@ -6,14 +6,13 @@ Neural network kernel function.
 ```math
     κ(x, y) =  asin(x' * y / sqrt[(1 + x' * x) * (1 + y' * y)])
 ```
-# Significance:
-Neal(1996)  pursued the limits of large models, and showed that a Bayesian neural network becomes a Gaussian process with a **neural network kernel** as the number of units approaches infinity.
+# Significance
+Neal (1996) pursued the limits of large models, and showed that a Bayesian neural network becomes a Gaussian process with a **neural network kernel** as the number of units approaches infinity. Here we give the neural network kernel for single hidden layer Bayesian neural network with erf (Error Function) as activation function
 
 # References:
-- Gaussian Processes for Machine Learning Pg 105 - http://www.gaussianprocess.org/gpml/chapters/RW4.pdf
-- Neal(1996) - https://www.cs.toronto.edu/~radford/bnn.book.html
-- Andrew Gordon's Thesis Pg 45 - http://www.cs.cmu.edu/~andrewgw/andrewgwthesis.pdf
-
+- [Gaussian Processes for Machine Learning Pg 105](http://www.gaussianprocess.org/gpml/chapters/RW4.pdf)
+- [Neal(1996)](https://www.cs.toronto.edu/~radford/bnn.book.html)
+- [Andrew Gordon's Thesis Pg 45](http://www.cs.cmu.edu/~andrewgw/andrewgwthesis.pdf)
 """
 struct NeuralNetworkKernel <: BaseKernel end
 
