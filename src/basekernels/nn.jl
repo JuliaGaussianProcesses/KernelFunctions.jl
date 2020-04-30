b2979f3500143973df9428613ad7de737ac8c761
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-05-01 00:00:28 +0530

    Improve docstring style and remove wiener kernel's tests

--- src/basekernels/nn.jl
+++ src/basekernels/nn.jl
@@ -7,10 +7,13 @@ Neural network kernel function.
     κ(x, y) =  asin(x' * y / sqrt[(1 + x' * x) * (1 + y' * y)])
 ```
 # Significance
-Neal (1996) pursued the limits of large models, and showed that a Bayesian neural network becomes a Gaussian process with a **neural network kernel** as the number of units approaches infinity. Here, we give the neural network kernel for single hidden layer Bayesian neural network with erf (Error Function) as activation function
+Neal (1996) pursued the limits of large models, and showed that a Bayesian neural network
+becomes a Gaussian process with a **neural network kernel** as the number of units
+approaches infinity. Here, we give the neural network kernel for single hidden layer
+Bayesian neural network with erf (Error Function) as activation function.
 
 # References:
-- [Gaussian Processes for Machine Learning Pg 105](http://www.gaussianprocess.org/gpml/chapters/RW4.pdf)
+- [GPML Pg 105](http://www.gaussianprocess.org/gpml/chapters/RW4.pdf)
 - [Neal(1996)](https://www.cs.toronto.edu/~radford/bnn.book.html)
 - [Andrew Gordon's Thesis Pg 45](http://www.cs.cmu.edu/~andrewgw/andrewgwthesis.pdf)
 """
