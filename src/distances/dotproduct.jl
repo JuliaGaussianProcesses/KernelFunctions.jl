a6159e10123197a9ad73d733e3398fb43ae6f460
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-05-12 19:13:50 +0200

    Solved tests for dotproduct

--- src/distances/dotproduct.jl
+++ src/distances/dotproduct.jl
@@ -1,7 +1,7 @@
 struct DotProduct <: Distances.PreMetric end
 # struct DotProduct <: Distances.UnionSemiMetric end
 
-@inline function Distances._evaluate(::DotProduct, a::AbstractVector{T}, b::AbstractVector{T}) where {T}
+@inline function Distances._evaluate(::DotProduct, a::AbstractVector, b::AbstractVector)
     @boundscheck if length(a) != length(b)
         throw(DimensionMismatch("first array has length $(length(a)) which does not match the length of the second, $(length(b))."))
     end
