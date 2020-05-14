6875aee5c52770ac69787639fc3115fe6a90f941
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-05-14 17:52:27 +0200

    Created full adjoints for DotProduct and evaluate for Sinus

--- src/zygote_adjoints.jl
+++ src/zygote_adjoints.jl
@@ -4,6 +4,44 @@
   end
 end
 
+@adjoint function pairwise(d::DotProduct, X::AbstractMatrix, Y::AbstractMatrix; dims=2)
+  D = pairwise(d, X, Y; dims = dims)
+  if dims == 1
+      return D, Δ -> (nothing, Δ * Y, (X' * Δ)')
+  else
+      return D, Δ -> (nothing, (Δ * Y')', X * Δ)
+  end
+end
+
+@adjoint function pairwise(d::DotProduct, X::AbstractMatrix; dims=2)
+  D = pairwise(d, X; dims = dims)
+  if dims == 1
+      return D, Δ -> (nothing, 2 * Δ * X)
+  else
+      return D, Δ -> (nothing, 2 * X * Δ)
+  end
+end
+
+@adjoint function evaluate(s::Sinus, x::AbstractVector, y::AbstractVector)
+  d = (x - y)
+  sind = sinpi.(d)
+  val = sum(abs2, sind ./ s.r)
+  gradx = 2π .* cospi.(d) .* sind ./ (s.r .^ 2)
+  val, Δ -> begin
+    ((r = -2Δ .* abs2.(sind) ./ s.r,), Δ * gradx, - Δ * gradx)
+  end
+end
+
+@adjoint function pairwise(s::Sinus, X::AbstractMatrix, Y::AbstractMatrix; dims=2)
+    D = pairwise(d, X, Y; dims = dims)
+    throw(error("Sinus metric has no defined adjoint for now... PR welcome!"))
+end
+
+@adjoint function pairwise(s::Sinus, X::AbstractMatrix; dims=2)
+  D = pairwise(d, X; dims = dims)
+  throw(error("Sinus metric has no defined adjoint for now... PR welcome!"))
+end
+
 @adjoint function loggamma(x)
     first(logabsgamma(x)) , Δ -> (Δ .* polygamma(0, x), )
 end
@@ -36,10 +74,10 @@ end
     return RowVecs(X), back
 end
 
-# @adjoint function evaluate(s::Sinus, x::AbstractVector, y::AbstractVector)
-#   d = evaluate(s, x, y)
-#   s = sum(sin.(π*(x-y)))
-#   d, Δ -> begin
-#     (Sinus(Δ ./ s.r), 2Δ .* cos.(x - y) * d, -2Δ .* cos.(x - y) * d)
-#   end
-# end
+@adjoint function Base.map(t::Transform, X::ColVecs)
+    pullback(_map, t, X)
+end
+
+@adjoint function Base.map(t::Transform, X::RowVecs)
+    pullback(_map, t, X)
+end
