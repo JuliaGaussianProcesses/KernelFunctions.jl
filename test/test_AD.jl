f70adc129e2595938c3320309dd793fc9167a0ae
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-05-14 17:51:09 +0200

    Created two function for testing any kernel, any AD and compare with FiniteDifferences.jl

--- test/test_AD.jl
+++ test/test_AD.jl
@@ -1,102 +1,19 @@
 using KernelFunctions
 using KernelFunctions: kappa, ColVecs, RowVecs
-using Flux: params
-import Zygote, ForwardDiff, ReverseDiff
-using Zygote: pullback
+import Zygote, ForwardDiff, ReverseDiff, FiniteDifferences
 using Test, LinearAlgebra, Random
-using FiniteDifferences
 
 include("utils_AD.jl")
-
-dims = [3, 3]
-ν = 3.0
-
-rng = MersenneTwister(42)
-
-A = rand(rng, dims...)
-B = rand(rng, dims...)
-K = [zeros(dims[1], dims[1]), zeros(dims[2], dims[2])]
-
-x = rand(rng, dims[1])
-y = rand(rng, dims[1])
-
-l = rand(rng)
-vl = l * ones(dims[1])
-
-kernels = [
-    SqExponentialKernel(),
-    ExponentialKernel(),
-    MaternKernel(ν = ν),
-    transform(SqExponentialKernel(), l),
-    transform(SqExponentialKernel(), vl),
-    ExponentiatedKernel() + LinearKernel(),
-    2.0 * PolynomialKernel() * Matern32Kernel(),
-]
-
-ds = log.([eps(), rand(rng)])
-
-testfunction(k, A, B, dim) = det(kernelmatrix(k, A, B, obsdim = dim))
-testfunction(k, A, dim) = det(kernelmatrix(k, A, obsdim = dim))
 ADs = [:Zygote, :ForwardDiff, :ReverseDiff]
 
-
-## Test kappa functions
-
-@testset "Kappa functions" begin
-    for k in kernels[isa.(kernels, KernelFunctions.SimpleKernel)]
-        @testset "$k" begin
-            @test_nowarn gradient(Val(:FiniteDiff), x -> kappa(k, exp(x[1])), ds[1]) # Check FiniteDiff does the right thing
-            for AD in ADs
-                @testset "$AD" begin
-                    for d in ds
-                        @test_nowarn gradient(Val(AD), x -> kappa(k, exp(x[1])), [d])
-                        @test gradient(Val(AD), x -> kappa(k, exp(x[1])), [d]) ≈ gradient(Val(:FiniteDiff), x -> kappa(k, exp(x[1])), [d]) atol=1e-8
-                    end
-                end
-            end
-        end
-    end
-end
-
-@testset "Kernel evaluations" begin
-    for k in kernels
-        @testset "$k" begin
-            for AD in ADs
-                @test_nowarn gradient(Val(:FiniteDiff), x -> k(x, y), x)
-                @testset "$AD" begin
-                    for d in ds
-                        @test_nowarn gradient(Val(AD), x -> k(x, y), x)
-                        @test gradient(Val(AD), x -> k(x, y), x) ≈ gradient(Val(:FiniteDiff), x -> k(x, y), x) atol=1e-8
-                    end
-                end
-            end
-        end
-    end
-end
-
-@testset "Kernel Matrices" begin
-    for k in kernels
-        @testset "$k" begin
-            for AD in ADs
-                # @test_nowarn gradient(Val(:FiniteDiff), x -> k(x, y), )
-                @testset "$AD" begin
-                    for dim in [1,2]
-                        @test_nowarn gradient(Val(AD), x -> testfunction(k, x, dim), A)
-                        @test_nowarn gradient(Val(AD), x -> testfunction(k, x, B, dim), A)
-                        @test gradient(Val(AD), x -> testfunction(k, x, B, dim), A) ≈ gradient(Val(:FiniteDiff), x -> testfunction(k, x, B, dim), A) atol=1e-8
-                        @test gradient(Val(AD), x -> testfunction(k, x, dim), A) ≈ gradient(Val(:FiniteDiff), x -> testfunction(k, x, dim), A) atol=1e-8
-                    end
-                end
-            end
-        end
-    end
-end
-
-@testset "Params differentiation" begin
-    for k in kernels
-        @testset "$k" begin
-            ps = params(k)
-            @test_nowarn gradient(Val(:Zygote), () -> k(x, y), ps)
-        end
+kname = "SEKernel_lengthscale"
+kfunction = () -> SEKernel()
+kfunction = (l -> transform(SEKernel(), first(l)))
+# args = nothing
+args = [2.0]
+v = test_FiniteDiff(kname, kfunction, args)
+if !v.anynonpass
+    for AD in ADs
+        test_AD(AD, kname, kfunction, args)
     end
 end
--- test/utils_AD.jl
+++ test/utils_AD.jl
@@ -1,4 +1,4 @@
-FDM = central_fdm(5, 1)
+FDM = FiniteDifferences.central_fdm(5, 1)
 
 function gradient(::Val{:Zygote}, f::Function, args)
     first(Zygote.gradient(f, args))
@@ -18,4 +18,81 @@ end
 
 function gradient(::Val{:FiniteDiff}, f::Function, args)
     first(FiniteDifferences.grad(FDM, f, args))
+end
+
+
+testfunction(k, A, B, dim) = sum(kernelmatrix(k, A, B, obsdim = dim))
+testfunction(k, A, dim) = sum(kernelmatrix(k, A, obsdim = dim))
+
+function test_FiniteDiff(kernelname, kernelfunction, args = nothing)
+    # Init arguments :
+    k = if args === nothing
+        kernelfunction()
+    else
+        kernelfunction(args)
+    end
+    dims = [3, 3]
+    rng = MersenneTwister(42)
+    @testset "FiniteDifferences with $(kernelname)" begin
+        if k isa SimpleKernel
+            for d in log.([eps(), rand(rng)])
+                @test_nowarn gradient(Val(:FiniteDiff), x -> kappa(k, exp(first(x))), [d])
+            end
+        end
+        ## Testing Kernel Functions
+        x = rand(rng, dims[1])
+        y = rand(rng, dims[1])
+        @test_nowarn gradient(Val(:FiniteDiff), x -> k(x, y), x)
+        if !(args === nothing)
+            @test_nowarn gradient(Val(:FiniteDiff), p -> kernelfunction(p)(x, y), args)
+        end
+        ## Testing Kernel Matrices
+        A = rand(rng, dims...)
+        B = rand(rng, dims...)
+        for dim in 1:2
+            @test_nowarn gradient(Val(:FiniteDiff), a -> testfunction(k, a, dim), A)
+            @test_nowarn gradient(Val(:FiniteDiff), a -> testfunction(k, a, B, dim), A)
+            @test_nowarn gradient(Val(:FiniteDiff), b -> testfunction(k, A, b, dim), B)
+            if !(args === nothing)
+                @test_nowarn gradient(Val(:FiniteDiff), p -> testfunction(kernelfunction(p), A, B, dim), args)
+            end
+        end
+    end
+end
+
+function test_AD(AD, kernelname, kernelfunction, args = nothing)
+    @testset "Testing $(kernelname) with AD : $(AD)" begin
+        # Test kappa function
+        dims = [3, 3]
+        k = if args === nothing
+            kernelfunction()
+        else
+            kernelfunction(args)
+        end
+        rng = MersenneTwister(42)
+        if k isa SimpleKernel
+            for d in log.([eps(), rand(rng)])
+                @test gradient(Val(AD), x -> kappa(k, exp(x[1])), [d]) ≈ gradient(Val(:FiniteDiff), x -> kappa(k, exp(x[1])), [d]) atol=1e-8
+            end
+        end
+        # Testing kernel evaluations
+        x = rand(rng, dims[1])
+        y = rand(rng, dims[1])
+        @test gradient(Val(AD), x -> k(x, y), x) ≈ gradient(Val(:FiniteDiff), x -> k(x, y), x) atol=1e-8
+        @test gradient(Val(AD), y -> k(x, y), y) ≈ gradient(Val(:FiniteDiff), y -> k(x, y), y) atol=1e-8
+        if !(args === nothing)
+            @test gradient(Val(AD), p -> kernelfunction(p)(x,y), args) ≈ gradient(Val(:FiniteDiff), p -> kernelfunction(p)(x, y), args) atol=1e-8
+        end
+        # Testing kernel matrices
+        A = rand(rng, dims...)
+        B = rand(rng, dims...)
+        for dim in 1:2
+            @test gradient(Val(AD), x -> testfunction(k, x, dim), A) ≈ gradient(Val(:FiniteDiff), x -> testfunction(k, x, dim), A) atol=1e-8
+            @test gradient(Val(AD), a -> testfunction(k, a, B, dim), A) ≈ gradient(Val(:FiniteDiff), a -> testfunction(k, a, B, dim), A) atol=1e-8
+            @test gradient(Val(AD), b -> testfunction(k, A, b, dim), B) ≈ gradient(Val(:FiniteDiff), b -> testfunction(k, A, b, dim), B) atol=1e-8
+            if !(args === nothing)
+                @test gradient(Val(AD), p -> testfunction(kernelfunction(p), A, dim), args) ≈ gradient(Val(:FiniteDiff), p -> testfunction(kernelfunction(p), A, dim), args) atol=1e-8
+            end
+        end
+    end
 end
