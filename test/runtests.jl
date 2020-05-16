5b2e580c26e267552a9fe2fb5fb50faf290d3d86
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-05-16 19:05:32 +0200

    Adding needed export

--- test/runtests.jl
+++ test/runtests.jl
@@ -8,7 +8,7 @@ using SpecialFunctions
 using Test
 import Zygote, ForwardDiff, ReverseDiff, FiniteDifferences
 
-using KernelFunctions: metric, kappa
+using KernelFunctions: metric, kappa, ColVecs, RowVecs
 
 # Writing tests:
 # 1. The file structure of the test should match precisely the file structure of src.
