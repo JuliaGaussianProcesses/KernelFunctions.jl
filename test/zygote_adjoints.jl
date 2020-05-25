6b5ba4d945bc136788d13fdeafed31411d3982f0
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-05-25 13:49:25 +0200

    Corrected Tests Zygote Adjoints

--- test/zygote_adjoints.jl
+++ test/zygote_adjoints.jl
@@ -5,17 +5,37 @@
     y = rand(rng, 5)
     r = rand(rng, 5)
 
-    gzeucl = gradient(Val(:Zygote), xy -> evaluate(Euclidean(), xy[1], xy[2]), [x,y])
-    gzsqeucl =  gradient(Val(:Zygote), xy -> evaluate(SqEuclidean(), xy[1], xy[2]), [x,y])
-    gzdotprod = gradient(Val(:Zygote), xy -> evaluate(KernelFunctions.DotProduct(), xy[1], xy[2]), [x,y])
-    gzdelta = gradient(Val(:Zygote), xy -> evaluate(KernelFunctions.Delta(), xy[1], xy[2]), [x,y])
-    gzsinus = gradient(Val(:Zygote), xy -> evaluate(KernelFunctions.Sinus(r), xy[1], xy[2]), [x,y])
+    gzeucl = gradient(:Zygote, [x,y]) do xy
+        evaluate(Euclidean(), xy[1], xy[2])
+    end
+    gzsqeucl = gradient(:Zygote, [x,y]) do xy
+        evaluate(SqEuclidean(), xy[1], xy[2])
+    end
+    gzdotprod = gradient(:Zygote, [x,y]) do xy
+        evaluate(KernelFunctions.DotProduct(), xy[1], xy[2])
+    end
+    gzdelta = gradient(:Zygote, [x,y]) do xy
+        evaluate(KernelFunctions.Delta(), xy[1], xy[2])
+    end
+    gzsinus = gradient(:Zygote, [x,y]) do xy
+        evaluate(KernelFunctions.Sinus(r), xy[1], xy[2])
+    end
 
-    gfeucl = gradient(Val(:FiniteDiff), xy -> evaluate(Euclidean(), xy[1], xy[2]), [x,y])
-    gfsqeucl = gradient(Val(:FiniteDiff), xy -> evaluate(SqEuclidean(), xy[1], xy[2]), [x,y])
-    gfdotprod = gradient(Val(:FiniteDiff), xy -> evaluate(KernelFunctions.DotProduct(), xy[1], xy[2]), [x,y])
-    gfdelta = gradient(Val(:FiniteDiff), xy -> evaluate(KernelFunctions.Delta(), xy[1], xy[2]), [x,y])
-    gfsinus = gradient(Val(:FiniteDiff), xy -> evaluate(KernelFunctions.Sinus(r), xy[1], xy[2]), [x,y])
+    gfeucl = gradient(:FiniteDiff, [x,y]) do xy
+        evaluate(Euclidean(), xy[1], xy[2])
+    end
+    gfsqeucl = gradient(:FiniteDiff, [x,y]) do xy
+        evaluate(SqEuclidean(), xy[1], xy[2])
+    end
+    gfdotprod = gradient(:FiniteDiff, [x,y]) do xy
+        evaluate(KernelFunctions.DotProduct(), xy[1], xy[2])
+    end
+    gfdelta = gradient(:FiniteDiff, [x,y]) do xy
+        evaluate(KernelFunctions.Delta(), xy[1], xy[2])
+    end
+    gfsinus = gradient(:FiniteDiff, [x,y]) do xy
+        evaluate(KernelFunctions.Sinus(r), xy[1], xy[2])
+    end
 
 
     @test all(gzeucl .≈ gfeucl)
