53096792537fb25fa3d423066e0dce51fb7ead7b
Theo Galy-Fajou <theo.galyfajou@gmail.com>
2020-06-22 19:04:56 +0200

    Made separate pairwise for KernelFunctions.jl

--- src/distances/pairwise.jl
+++ src/distances/pairwise.jl
@@ -1,48 +1,44 @@
-# Add the possibility to use Distances.pairwise on vectors as well
+# Add our own pairwise function to be able to apply it on vectors
 
-function Distances.pairwise(
-    d::PreMetric, 
-    X::AbstractVector, 
-    Y::AbstractVector,
-)
+function pairwise(d::PreMetric, X::AbstractVector, Y::AbstractVector)
     broadcast(d, X, Y')
 end
 
-Distances.pairwise(d::PreMetric, X::AbstractVector) = pairwise(d, X, X)
+pairwise(d::PreMetric, X::AbstractVector) = pairwise(d, X, X)
 
-function Distances.pairwise!(
-    out::AbstractMatrix, 
-    d::PreMetric, 
-    X::AbstractVector, 
+function pairwise!(
+    out::AbstractMatrix,
+    d::PreMetric,
+    X::AbstractVector,
     Y::AbstractVector,
 )
     broadcast!(d, out, X, Y')
 end
 
-Distances.pairwise!(out::AbstractMatrix, d::PreMetric, X::AbstractVector) = pairwise!(out, d, X, X)
+pairwise!(out::AbstractMatrix, d::PreMetric, X::AbstractVector) =
+    pairwise!(out, d, X, X)
 
-# This is type piracy. We should not doing this.
-function Distances.pairwise(d::PreMetric, x::AbstractVector{<:Real})
-    return pairwise(d, reshape(x, :, 1); dims=1)
+function pairwise(d::PreMetric, x::AbstractVector{<:Real})
+    return Distances.pairwise(d, reshape(x, :, 1); dims = 1)
 end
 
-function Distances.pairwise(
+function pairwise(
     d::PreMetric,
     x::AbstractVector{<:Real},
     y::AbstractVector{<:Real},
 )
-    return pairwise(d, reshape(x, :, 1), reshape(y, :, 1); dims=1)
+    return Distances.pairwise(d, reshape(x, :, 1), reshape(y, :, 1); dims = 1)
 end
 
-function Distances.pairwise!(out::AbstractMatrix, d::PreMetric, x::AbstractVector{<:Real})
-    return pairwise!(out, d, reshape(x, :, 1); dims=1)
+function pairwise!(out::AbstractMatrix, d::PreMetric, x::AbstractVector{<:Real})
+    return Distances.pairwise!(out, d, reshape(x, :, 1); dims = 1)
 end
 
-function Distances.pairwise!(
+function pairwise!(
     out::AbstractMatrix,
     d::PreMetric,
     x::AbstractVector{<:Real},
     y::AbstractVector{<:Real},
 )
-    return pairwise!(out, d, reshape(x, :, 1), reshape(y, :, 1); dims=1)
+    return Distances.pairwise!(out, d, reshape(x, :, 1), reshape(y, :, 1); dims=1)
 end
--- test/distances/pairwise.jl
+++ test/distances/pairwise.jl
@@ -8,9 +8,9 @@
     Y = hcat(y...)
     K = zeros(4, 5)
 
-    @test pairwise(d, x, y) ≈ pairwise(d, X, Y, dims=2)
-    @test pairwise(d, x) ≈ pairwise(d, X, dims=2)
-    pairwise!(K, d, x, y)
+    @test KernelFunctions.pairwise(d, x, y) ≈ pairwise(d, X, Y, dims=2)
+    @test KernelFunctions.pairwise(d, x) ≈ pairwise(d, X, dims=2)
+    KernelFunctions.pairwise!(K, d, x, y)
     @test K ≈ pairwise(d, X, Y, dims=2)
 
     x = randn(rng, 10)
@@ -18,9 +18,9 @@
     y = randn(rng, 11)
     Y = reshape(y, :, 1)
     K = zeros(10, 11)
-    @test pairwise(d, x, y) ≈ pairwise(d, X, Y; dims=1)
-    @test pairwise(d, x) ≈ pairwise(d, X; dims=1)
-    pairwise!(K, d, x, y)
+    @test KernelFunctions.pairwise(d, x, y) ≈ pairwise(d, X, Y; dims=1)
+    @test KernelFunctions.pairwise(d, x) ≈ pairwise(d, X; dims=1)
+    KernelFunctions.pairwise!(K, d, x, y)
     @test K ≈ pairwise(d, X, Y, dims=1)
 
 
