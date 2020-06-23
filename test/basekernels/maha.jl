6fa0bda6e2c16386476ca2bf0978bd642c582960
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2020-06-23 12:22:55 +0200

    Replace constructor of Maha with keyword one

--- test/basekernels/maha.jl
+++ test/basekernels/maha.jl
@@ -5,12 +5,12 @@
     v2 = rand(rng, 3)
 
     P = rand(rng, 3, 3)
-    k = MahalanobisKernel(P)
+    k = MahalanobisKernel(P=P)
 
     @test kappa(k, x) == exp(-x)
     @test k(v1, v2) ≈ exp(-sqmahalanobis(v1, v2, P))
     @test kappa(ExponentialKernel(), x) == kappa(k, x)
     @test repr(k) == "Mahalanobis Kernel (size(P) = $(size(P)))"
-    # test_ADs(P -> MahalanobisKernel(P), P)
+    # test_ADs(P -> MahalanobisKernel(P=P), P)
     @test_broken "Nothing passes (problem with Mahalanobis distance in Distances)"
 end
