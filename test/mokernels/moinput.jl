7aad3159ebeec13c60ad08ec70b1f3ab18b2e12b
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-07-22 22:29:26 +0530

    Update Tests

--- test/mokernels/moinput.jl
+++ test/mokernels/moinput.jl
@@ -1,17 +1,17 @@
 @testset "moinput" begin
     
-    x =  rand(5)
+    x =  [rand(5) for _ in 1:4]
     mgpi = MOInput(x, 3)
     
-    @test length(mgpi) == 15
-    @test size(mgpi) == (15,)
-    @test size(mgpi, 1) == 15
+    @test length(mgpi) == 12
+    @test size(mgpi) == (12,)
+    @test size(mgpi, 1) == 12
     @test size(mgpi, 2) == 1
-    @test lastindex(mgpi) == 15
+    @test lastindex(mgpi) == 12
     @test firstindex(mgpi) == 1
     @test mgpi[2] == (x[2], 1)
-    @test mgpi[5] == (x[5], 1)
-    @test mgpi[7] == (x[2], 2)
+    @test mgpi[5] == (x[1], 2)
+    @test mgpi[7] == (x[3], 2)
     @test all([(x_, i) for i in 1:3 for x_ in x ] .== mgpi)
 
 end
