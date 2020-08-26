e81cb0166904891957cb12803c71a623bac96fa4
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-08-26 13:55:40 +0530

    Make maha tests more readable

--- test/basekernels/maha.jl
+++ test/basekernels/maha.jl
@@ -18,12 +18,23 @@
     fdm = FiniteDifferences.Central(5, 1);
     
     
-    FiniteDifferences.to_vec(dist::SqMahalanobis{Float64}) = vec(dist.qmat), x -> SqMahalanobis(reshape(x, size(dist.qmat)...))
-
-    @test_broken j′vp(fdm, x -> MahalanobisKernel(Array(x[1]'*x[1]))(x[2], x[3]), 1, [U, v1, v2]) ≈
-    Zygote.pullback(x -> MahalanobisKernel(Array(x[1]'*x[1]))(x[2], x[3]), [U, v1, v2])[2](1)
-    @test all(j′vp(fdm, x -> SqMahalanobis(Array(x[1]'*x[1]))(x[2], x[3]), 1, [U, v1, v2])[1][1] .≈ 
-    Zygote.pullback(x -> SqMahalanobis(Array(x[1]'*x[1]))(x[2], x[3]), [U, v1, v2])[2](1)[1][1])
+    function FiniteDifferences.to_vec(dist::SqMahalanobis{Float64})
+        return vec(dist.qmat), x -> SqMahalanobis(reshape(x, size(dist.qmat)...))
+    end
+    
+    function test_mahakernel(U::UpperTriangular, v1::AbstractVector, v2::AbstractVector)
+        return MahalanobisKernel(Array(U'*U))(v1, v2)
+    end
+    
+    @test_broken all(j′vp(fdm, test_mahakernel, 1, U, v1, v2) .≈
+        Zygote.pullback(test_mahakernel, U, v1, v2)[2](1))
+    
+    function test_sqmaha(U::UpperTriangular, v1::AbstractVector, v2::AbstractVector)
+        return SqMahalanobis(Array(U'*U))(v1, v2)
+    end
+    
+    @test_broken all(j′vp(fdm, test_sqmaha, 1, U, v1, v2) .≈ 
+    Zygote.pullback(test_sqmaha, U, v1, v2)[2](1))
     
     # test_ADs(U -> MahalanobisKernel(Array(U' * U)), U, ADs=[:Zygote])
     @test_broken "Nothing passes (problem with Mahalanobis distance in Distances)"
