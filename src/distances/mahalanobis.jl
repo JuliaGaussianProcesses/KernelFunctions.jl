180c934ea290bfe1a782154444fce2a70200e434
Will Tebbutt <will.tebbutt@invenialabs.co.uk>
2020-08-30 19:19:20 +0100

    Remove redundant file

--- src/distances/mahalanobis.jl
+++
@@ -1,2 +0,0 @@
-Distances.pairwise(d::SqMahalanobis, x::ColVecs, y::ColVecs) = pairwise(d, x.X, y.X; dims=2)
-Distances.pairwise(d::SqMahalanobis, x::RowVecs, y::RowVecs) = pairwise(d, x.X, y.X; dims=1)
