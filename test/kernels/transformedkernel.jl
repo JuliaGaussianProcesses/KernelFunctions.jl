cfdb0339b36a70a25b99e7a0b96c0b0e4b929966
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-08-31 21:57:02 +0530

    Fix AD issue with mixed input for TransformedKernel (#160)
    * Fix mixed input bug for TransformedKernel
    
    * Fix tests
    
    * Support mized input in pairwise
    
    * avoid splatting
    
    * style fixes
    
    * use permute dims

--- test/kernels/transformedkernel.jl
+++ test/kernels/transformedkernel.jl
@@ -50,6 +50,22 @@
             tmp_diag = Vector{Float64}(undef, length(x))
             @test kerneldiagmatrix!(tmp_diag, kt, x) ≈ kerneldiagmatrix(kt, x)
         end
+
+        @testset "mixed inputs" begin
+            k = transform(SqExponentialKernel(), 10.0)
+            x = rand(rng, 5, 3)
+            X1 = collect(eachcol(x))
+            Y1 = KernelFunctions.ColVecs(x)
+            @test_nowarn Zygote.gradient(k-> sum(kernelmatrix(k, X1, Y1)), k)
+            @test_nowarn Zygote.gradient(k-> sum(kernelmatrix(k, Y1, X1)), k)
+            @test kernelmatrix(k, X1, Y1) ≈ kernelmatrix(k, X1, X1) ≈ kernelmatrix(k, Y1, Y1)
+
+            X2 = collect(eachrow(x))
+            Y2 = KernelFunctions.RowVecs(x)
+            @test_nowarn Zygote.gradient(k-> sum(kernelmatrix(k, X2, Y2)), k)
+            @test_nowarn Zygote.gradient(k-> sum(kernelmatrix(k, Y2, X2)), k)
+            @test kernelmatrix(k, X2, Y2) ≈ kernelmatrix(k, X2, X2) ≈ kernelmatrix(k, Y2, Y2)
+        end
     end
     test_ADs(x->transform(SqExponentialKernel(), x[1]), rand(1))# ADs = [:ForwardDiff, :ReverseDiff])
     # Test implicit gradients
