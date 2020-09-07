acdec1a688a5ce40c6259e7efbf9f5c2ced04539
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-09-07 14:43:47 +0530

    Fix maha tests

--- test/basekernels/maha.jl
+++ test/basekernels/maha.jl
@@ -21,20 +21,21 @@
     function FiniteDifferences.to_vec(dist::SqMahalanobis{Float64})
         return vec(dist.qmat), x -> SqMahalanobis(reshape(x, size(dist.qmat)...))
     end
+    a = rand()
     
     function test_mahakernel(U::UpperTriangular, v1::AbstractVector, v2::AbstractVector)
         return MahalanobisKernel(P=Array(U'*U))(v1, v2)
     end
     
-    @test_broken all(j′vp(fdm, test_mahakernel, 1, U, v1, v2) .≈
-        Zygote.pullback(test_mahakernel, U, v1, v2)[2](1))
+    @test all(FiniteDifferences.j′vp(fdm, test_mahakernel, a, U, v1, v2)[1] .≈
+        UpperTriangular(Zygote.pullback(test_mahakernel, U, v1, v2)[2](a)[1]))
     
     function test_sqmaha(U::UpperTriangular, v1::AbstractVector, v2::AbstractVector)
         return SqMahalanobis(Array(U'*U))(v1, v2)
     end
     
-    @test_broken all(j′vp(fdm, test_sqmaha, 1, U, v1, v2) .≈ 
-    Zygote.pullback(test_sqmaha, U, v1, v2)[2](1))
+    @test all(FiniteDifferences.j′vp(fdm, test_sqmaha, a, U, v1, v2)[1] .≈ 
+    UpperTriangular(Zygote.pullback(test_sqmaha, U, v1, v2)[2](a)[1]))
     
     # test_ADs(U -> MahalanobisKernel(P=Array(U' * U)), U, ADs=[:Zygote])
     @test_broken "Nothing passes (problem with Mahalanobis distance in Distances)"
