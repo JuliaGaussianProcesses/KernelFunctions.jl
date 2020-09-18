dcdedb41eb4eee9417a65fe78910a361bf52035f
Sharan Yalburgi <sharanyalburgi@gmail.com>
2020-09-18 16:17:07 +0530

    Add @boundscheck. Remove explicit definition of length and iterate

--- src/mokernels/moinput.jl
+++ src/mokernels/moinput.jl
@@ -8,24 +8,15 @@ struct MOInput{T<:AbstractVector} <: AbstractVector{Tuple{Any,Int}}
     out_dim::Integer
 end
 
-Base.length(inp::MOInput) = inp.out_dim * length(inp.x)
-
-Base.size(inp::MOInput, d) = d::Integer == 1 ? inp.out_dim * size(inp.x, 1) : 1 
 Base.size(inp::MOInput) = (inp.out_dim * size(inp.x, 1),)
 
-Base.lastindex(inp::MOInput) = length(inp)
-Base.firstindex(inp::MOInput) = 1
-
 function Base.getindex(inp::MOInput, ind::Integer)
-    if ind > 0
-        out_dim = ind ÷ length(inp.x) + 1
-        ind = ind % length(inp.x)
-        if ind==0 ind = length(inp.x); out_dim-=1 end
-        return (inp.x[ind], out_dim::Int)
-    else
+    @boundscheck if ind <= 0 || ind > length(inp)
         throw(BoundsError(string("Trying to access at ", ind)))
-    end
+    end 
+    
+    out_dim = ind ÷ length(inp.x) + 1
+    ind = ind % length(inp.x)
+    if ind==0 ind = length(inp.x); out_dim-=1 end
+    return (inp.x[ind], out_dim::Int)
 end
-
-Base.iterate(inp::MOInput) = (inp[1], 1)
-Base.iterate(inp::MOInput, state) = (state<length(inp)) ? (inp[state + 1], state + 1) : nothing
