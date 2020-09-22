bb4cfe552d5e296bce281c5328ce9b2c54f1b854
wt <will.tebbutt@invenialabs.co.uk>
2020-09-22 22:53:56 +0100

    Use vectors rather than Refs

--- src/transform/periodic_transform.jl
+++ src/transform/periodic_transform.jl
@@ -4,24 +4,26 @@
 Makes a kernel periodic by mapping a scalar input onto the unit circle. Samples from a GP
 with a kernel with this transformation applied will produce samples with frequenct `f`.
 """
-struct PeriodicTransform{Tf<:Ref{<:Real}} <: Transform
+struct PeriodicTransform{Tf<:AbstractVector{<:Real}} <: Transform
     f::Tf
 end
 
 @functor PeriodicTransform
 
-PeriodicTransform(f::Real) = PeriodicTransform(Ref(f))
+PeriodicTransform(f::Real) = PeriodicTransform([f])
 
 dim(t::PeriodicTransform) = 2
 
-(t::PeriodicTransform)(x::Real) = [sinpi((2 * t.f[]) * x), cospi((2 * t.f[]) * x)]
+(t::PeriodicTransform)(x::Real) = [sinpi(2 * first(t.f) * x), cospi(2 * first(t.f) * x)]
 
 function _map(t::PeriodicTransform, x::AbstractVector{<:Real})
-    return RowVecs(hcat(sinpi.((2 * t.f[]) .* x), cospi.((2 * t.f[]) .* x)))
+    return RowVecs(hcat(sinpi.((2 * first(t.f)) .* x), cospi.((2 * first(t.f)) .* x)))
 end
 
-Base.isequal(t1::PeriodicTransform, t2::PeriodicTransform) = isequal(t1.f[], t2.f[])
+function Base.isequal(t1::PeriodicTransform, t2::PeriodicTransform)
+    return isequal(first(t1.f), first(t2.f))
+end
 
 function Base.show(io::IO, t::PeriodicTransform)
-    print(io, "Periodic Transform with frequency $(t.f[])")
+    print(io, "Periodic Transform with frequency $(first(t.f))")
 end
