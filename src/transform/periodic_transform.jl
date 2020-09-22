0fc5dbd9aa7ae5332c7a6d134d4094e5cb95101a
wt <will.tebbutt@invenialabs.co.uk>
2020-09-22 17:47:52 +0100

    Improve docstring

--- src/transform/periodic_transform.jl
+++ src/transform/periodic_transform.jl
@@ -1,7 +1,8 @@
 """
-    PeriodicTransform{Tf<:Ref{<:Real}} <: Transform
+    PeriodicTransform(f)
 
-Makes a kernel periodic by transforming an input to be periodic.
+Makes a kernel periodic by mapping a scalar input onto the unit circle. Samples from a GP
+with a kernel with this transformation applied will produce samples with frequenct `f`.
 """
 struct PeriodicTransform{Tf<:Ref{<:Real}} <: Transform
     f::Tf
@@ -13,7 +14,7 @@ PeriodicTransform(f::Real) = PeriodicTransform(Ref(f))
 
 dim(t::PeriodicTransform) = 2
 
-(t::PeriodicTransform)(x::Real) = [cosp((2 * t.f[]) * x), sinpi((2 * t.f[]) * x)]
+(t::PeriodicTransform)(x::Real) = [cospi((2 * t.f[]) * x), sinpi((2 * t.f[]) * x)]
 
 function _map(t::PeriodicTransform, x::AbstractVector{<:Real})
     return RowVecs(hcat(cospi.((2 * t.f[]) .* x), sinpi.((2 * t.f[]) .* x)))
