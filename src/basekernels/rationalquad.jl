7eaae6453fc2deaafb804ecc53e698545ba4072a
wt <will.tebbutt@invenialabs.co.uk>
2020-09-22 21:35:46 +0100

    Fix some style issues

--- src/basekernels/rationalquad.jl
+++ src/basekernels/rationalquad.jl
@@ -1,9 +1,9 @@
 """
-    RationalQuadraticKernel(; α = 2.0)
+    RationalQuadraticKernel(; α=2.0)
 
 The rational-quadratic kernel is a Mercer kernel given by the formula:
 ```
-    κ(x,y)=(1 + ||x − y||² / α)^(-α)
+    κ(x, y) = (1 + ||x − y||² / α)^(-α)
 ```
 where `α` is a shape parameter of the Euclidean distance. Check
 [`GammaRationalQuadraticKernel`](@ref) for a generalization.
@@ -24,20 +24,25 @@ end
 
 metric(::RationalQuadraticKernel) = SqEuclidean()
 
-Base.show(io::IO, κ::RationalQuadraticKernel) = print(io, "Rational Quadratic Kernel (α = ", first(κ.α), ")")
+function Base.show(io::IO, κ::RationalQuadraticKernel)
+    print(io, "Rational Quadratic Kernel (α = $(first(κ.α)))")
+end
 
 """
-`GammaRationalQuadraticKernel([ρ=1.0[,α=2.0[,γ=2.0]]])`
+`GammaRationalQuadraticKernel([α=2.0 [, γ=2.0]])`
+
 The Gamma-rational-quadratic kernel is an isotropic Mercer kernel given by the formula:
 ```
-    κ(x,y)=(1+ρ^(2γ)||x−y||^(2γ)/α)^(-α)
+    κ(x, y) = (1 + ||x−y||^(2γ) / α)^(-α)
 ```
 where `α` is a shape parameter of the Euclidean distance and `γ` is another shape parameter.
 """
 struct GammaRationalQuadraticKernel{Tα<:Real, Tγ<:Real} <: SimpleKernel
     α::Vector{Tα}
     γ::Vector{Tγ}
-    function GammaRationalQuadraticKernel(;alpha::Tα=2.0, gamma::Tγ=2.0, α::Tα=alpha, γ::Tγ=gamma) where {Tα<:Real, Tγ<:Real}
+    function GammaRationalQuadraticKernel(
+        ;alpha::Tα=2.0, gamma::Tγ=2.0, α::Tα=alpha, γ::Tγ=gamma,
+    ) where {Tα<:Real, Tγ<:Real}
         @check_args(GammaRationalQuadraticKernel, α, α > one(Tα), "α > 1")
         @check_args(GammaRationalQuadraticKernel, γ, γ >= one(Tγ), "γ >= 1")
         return new{Tα, Tγ}([α], [γ])
@@ -46,7 +51,12 @@ end
 
 @functor GammaRationalQuadraticKernel
 
-kappa(κ::GammaRationalQuadraticKernel, d²::T) where {T<:Real} = (one(T)+d²^first(κ.γ)/first(κ.α))^(-first(κ.α))
+function kappa(κ::GammaRationalQuadraticKernel, d²::Real)
+    return (one(d²) + d²^first(κ.γ) / first(κ.α))^(-first(κ.α))
+end
+
 metric(::GammaRationalQuadraticKernel) = SqEuclidean()
 
-Base.show(io::IO, κ::GammaRationalQuadraticKernel) = print(io, "Gamma Rational Quadratic Kernel (α = ", first(κ.α), ", γ = ", first(κ.γ), ")")
+function Base.show(io::IO, κ::GammaRationalQuadraticKernel)
+    print(io, "Gamma Rational Quadratic Kernel (α = $(first(κ.α)), γ = $(first(κ.γ)))")
+end
