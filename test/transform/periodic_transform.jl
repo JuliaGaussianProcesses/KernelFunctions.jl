f24f285fd6361f1eda49f9cb78e3a628a83cd68b
willtebbutt <wct23@cam.ac.uk>
2020-09-23 10:49:30 +0100

    Implement periodic transform (#173)
    * Implement periodic transform
    
    * Improve docstring
    
    * Flip sin-cos order
    
    * Use vectors rather than Refs
    
    * Update src/transform/periodic_transform.jl
    
    Co-authored-by: Letif Mones <letif.mones.research@gmail.com>
    
    * Bump patch
    
    Co-authored-by: wt <will.tebbutt@invenialabs.co.uk>
    Co-authored-by: Letif Mones <letif.mones.research@gmail.com>

---
+++ test/transform/periodic_transform.jl
@@ -0,0 +1,16 @@
+@testset "periodic_transform" begin
+    @testset "compare to periodic exponentiated quadratic" begin
+        rng = MersenneTwister(123456)
+        f = rand(rng) + 2.0
+        x = collect(range(0.0, 3.0 / f; length=1_000))
+
+        # Construct in the usual way.
+        k_eq_periodic = transform(PeriodicKernel(; r=[sqrt(0.25)]), f)
+
+        # Construct using the peridic transform.
+        k_eq_transform = transform(SqExponentialKernel(), PeriodicTransform(f))
+
+        @test kernelmatrix(k_eq_periodic, x) ≈ kernelmatrix(k_eq_transform, x)
+        # TODO - add interface_tests once #159 is merged.
+    end
+end
