ab492c0c4c9e4e1fcc8e3715a7349efef6d0229b
wt <will.tebbutt@invenialabs.co.uk>
2020-09-24 16:32:09 +0100

    Test FBM kernel

--- test/basekernels/fbm.jl
+++ test/basekernels/fbm.jl
@@ -7,7 +7,7 @@
     @test k(v1,v2) ≈ (sqeuclidean(v1, zero(v1))^h + sqeuclidean(v2, zero(v2))^h - sqeuclidean(v1-v2, zero(v1-v2))^h)/2 atol=1e-5
     @test repr(k) == "Fractional Brownian Motion Kernel (h = $(h))"
 
-
+    test_interface(k)
     @test repr(k) == "Fractional Brownian Motion Kernel (h = $(h))"
     test_ADs(FBMKernel, ADs = [:ReverseDiff, :Zygote])
     @test_broken "Tests failing for kernelmatrix(k, x) for ForwardDiff"
