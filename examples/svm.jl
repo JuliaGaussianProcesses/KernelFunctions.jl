0eb6f36c161d08491a82a9735b0d0b453b947968
ST John <st--@users.noreply.github.com>
2021-01-08 11:38:22 +0000

    cleanup svm.jl

--- examples/svm.jl
+++ examples/svm.jl
@@ -1,14 +1,23 @@
+# -*- coding: utf-8 -*-
 # # Support Vector Machines
-# ## Package loading
+
+# TODO: introduction
+#
+# We first load the packages we will need in this notebook:
+
 using KernelFunctions
 using Distributions, LinearAlgebra
 using Plots; default(legendfontsize = 15.0, ms = 5.0)
 
 # ## Data Generation
-# ### We first generate a mixture of two Gaussians in 2 dimensions
+#
+# We first generate a mixture of two Gaussians in 2 dimensions
+
 xmin = -3; xmax = 3 # Limits for sampling μ₁ and μ₂
 μ = rand(Uniform(xmin, xmax), 2, 2) # Sample 2 Random Centers
-# ### We then sample both y and x
+
+# We then sample both the input $x$ and the class $y$:
+
 N = 100 # Number of samples
 y = rand((-1, 1), N) # Select randomly between the two classes
 x = Vector{Vector{Float64}}(undef, N) # We preallocate x
@@ -19,27 +28,37 @@ scatter!(getindex.(x[y .== -1], 1), getindex.(x[y .== -1], 2), label = "y = 2")
 
 # ## Model Definition
 # TODO Write theory here
-# ### We create a kernel k
-k = SqExponentialKernel() # SqExponentialKernel/RBFKernel
+
+# ### Kernel
+
+k = SqExponentialKernel()  # SqExponentialKernel/RBFKernel
 λ = 1.0 # Regularization parameter
 
-# ### We create a function to return the optimal prediction for a
-# test data `x_new`
+# ### Predictor
+# We create a function to return the optimal prediction for a test data `x_new`
+
 function f(x_new, x, y, k, λ)
     kernelmatrix(k, x_new, x) * inv(kernelmatrix(k, x) + λ * I) * y # Optimal prediction f
 end
 
-# ### We also compute the total loss of the model that we want to minimize
+# ### Loss
+# We also compute the total loss of the model that we want to minimize
+
 hingeloss(y, ŷ) = maximum(zero(ŷ), 1 - y * ŷ) # hingeloss function
 function reg_hingeloss(k, x, y, λ)
     ŷ = f(x, x, y, k, λ)
     return sum(hingeloss.(y, ŷ)) - λ * norm(ŷ) # Total svm loss with regularisation
 end
-# ### We create a 2D grid based on the maximum values of the data
+
+# ### Testing the trained model
+# We create a 2D grid based on the maximum values of the data
+
 N_test = 100 # Size of the grid
 xgrid = range(extrema(vcat(x...)).*1.1..., length=N_test) # Create a 1D grid
 xgrid_v = vec(collect.(Iterators.product(xgrid, xgrid))) #Combine into a 2D grid
-# ### We predict the value of y on this grid on plot it against the data
+
+# We predict the value of y on this grid on plot it against the data
+
 y_grid = f(xgrid_v, x, y, k, λ) #Compute prediction on a grid
 contourf(xgrid, xgrid, reshape(y_grid, N_test, N_test)', label =  "Predictions", title="Trained model")
 scatter!(getindex.(x[y .== 1], 1), getindex.(x[y .== 1], 2), label = "y = 1")
