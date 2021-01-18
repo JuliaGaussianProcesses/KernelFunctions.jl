5e9e322e1bf2906fcf79c707e894f398391eafbe
ST John <st--@users.noreply.github.com>
2021-01-18 16:46:50 +0000

    make assert messages consistent

--- src/matrix/kernelkroneckermat.jl
+++ src/matrix/kernelkroneckermat.jl
@@ -3,7 +3,7 @@ using .Kronecker
 export kernelkronmat
 
 function kernelkronmat(κ::Kernel, X::AbstractVector, dims::Int)
-    @assert iskroncompatible(κ) "The chosen kernel is not compatible for kroenecker matrices (see [`iskroncompatible`](@ref))"
+    @assert iskroncompatible(κ) "The chosen kernel is not compatible with constructing Kronecker matrices (see [`iskroncompatible`](@ref))"
     k = kernelmatrix(κ, X)
     return kronecker(k, dims)
 end
@@ -11,14 +11,14 @@ end
 function kernelkronmat(
     κ::Kernel, X::AbstractVector{<:AbstractVector}; obsdim::Int=defaultobs
 )
-    @assert iskroncompatible(κ) "The chosen kernel is not compatible for Kronecker matrices"
+    @assert iskroncompatible(κ) "The chosen kernel is not compatible with constructing Kronecker matrices (see [`iskroncompatible`](@ref))"
     Ks = kernelmatrix.(κ, X)
     return K = reduce(⊗, Ks)
 end
 
 """
-    To be compatible with kroenecker constructions the kernel must satisfy
-    the property : for x,x' ∈ ℜᴰ
-    k(x,x') = ∏ᵢᴰ k(xᵢ,x'ᵢ)
+    To be compatible with Kronecker constructions the kernel must satisfy
+    the property: for x, x' ∈ ℜᴰ
+    k(x, x') = ∏ᵢᴰ k(xᵢ, x'ᵢ)
 """
 @inline iskroncompatible(κ::Kernel) = false # Default return for kernels
