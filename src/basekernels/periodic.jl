662e252256bc09a2d4fed78377dc9acab6025502
David Widmann <devmotion@users.noreply.github.com>
2021-01-19 03:34:32 +0100

    Fix typo

--- src/basekernels/periodic.jl
+++ src/basekernels/periodic.jl
@@ -5,7 +5,7 @@ Periodic kernel with parameter `r`.
 
 # Definition
 
-For inputs ``x, x' \\in \\mathbb{R}^d``, the period kernel with parameter ``r_i > 0`` is
+For inputs ``x, x' \\in \\mathbb{R}^d``, the periodic kernel with parameter ``r_i > 0`` is
 defined[^DM] as
 ```math
 k(x, x'; r) = \\exp\\bigg(- \\frac{1}{2} \\sum_{i=1}^d \\bigg(\\frac{\\sin\\big(\\pi(x_i - x'_i)\\big)}{r_i}\\bigg)^2\\bigg).
