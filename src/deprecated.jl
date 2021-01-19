5e6774d9568cdf2ac69a179eea685c25f60f300b
ST John <st--@users.noreply.github.com>
2021-01-19 11:31:01 +0000

    add deprecated kerneldiagmatrix[!]

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -7,3 +7,17 @@
 @deprecate PiecewisePolynomialKernel{V}(A::AbstractMatrix{<:Real}) where {V} transform(
     PiecewisePolynomialKernel{V}(size(A, 1)), LinearTransform(cholesky(A).U)
 )
+
+# TODO: remove tests when removed
+@deprecate function kerneldiagmatrix(κ::Kernel, X; obsdim::Int=2)
+    return kernelmatrix_diag(κ, X; obsdim=obsdim)
+end
+@deprecate function kerneldiagmatrix(κ::Kernel, X, Y; obsdim::Int=2)
+    return kernelmatrix_diag(κ, X, Y; obsdim=obsdim)
+end
+@deprecate function kerneldiagmatrix!(K::AbstractVector, κ::Kernel, X; obsdim::Int=2)
+    return kernelmatrix_diag!(K, κ, X; obsdim=obsdim)
+end
+@deprecate function kerneldiagmatrix!(K::AbstractVector, κ::Kernel, X, Y; obsdim::Int=2)
+    return kernelmatrix_diag!(K, κ, X, Y; obsdim=obsdim)
+end
