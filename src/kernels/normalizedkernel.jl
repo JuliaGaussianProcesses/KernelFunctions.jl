4804fbc1bc07e3d54b0c2b57c8d3773b6a789ae6
Ross Viljoen <ross@viljoen.co.uk>
2021-04-09 18:02:18 +0100

    Removed extraneous normalization functions

--- src/kernels/normalizedkernel.jl
+++ src/kernels/normalizedkernel.jl
@@ -10,7 +10,6 @@ kernel ``k`` is defined as
 ```math
 \\widetilde{k}(x, x'; k) = \\frac{k(x, x')}{\\sqrt{k(x, x) k(x', x')}}.
 ```
-
 """
 struct NormalizedKernel{Tk<:Kernel} <: Kernel
     kernel::Tk
@@ -21,32 +20,40 @@ end
 (κ::NormalizedKernel)(x, y) = κ.kernel(x, y) / sqrt(κ.kernel(x, x) * κ.kernel(y, y))
 
 function kernelmatrix(κ::NormalizedKernel, x::AbstractVector, y::AbstractVector)
-    return kernelmatrix(κ.kernel, x, y) ./ _normalizationmatrix(κ, x, y)
+    return kernelmatrix(κ.kernel, x, y) ./
+           sqrt.(
+        kernelmatrix_diag(κ.kernel, x) .* permutedims(kernelmatrix_diag(κ.kernel, y))
+    )
 end
 
 function kernelmatrix(κ::NormalizedKernel, x::AbstractVector)
-    return kernelmatrix(κ.kernel, x) ./ _normalizationmatrix(κ, x, x)
+    xdiag = kernelmatrix_diag(κ.kernel, x)
+    return kernelmatrix(κ.kernel, x) ./ sqrt.(xdiag .* permutedims(xdiag))
 end
 
 function kernelmatrix_diag(κ::NormalizedKernel, x::AbstractVector)
-    return kernelmatrix_diag(κ.kernel, x) ./ _normalizationmatrix_diag(κ, x, x)
+    xdiag = kernelmatrix_diag(κ.kernel, x)
+    return kernelmatrix_diag(κ.kernel, x) ./ sqrt.(xdiag .* xdiag)
 end
 
 function kernelmatrix_diag(κ::NormalizedKernel, x::AbstractVector, y::AbstractVector)
-    return kernelmatrix_diag(κ.kernel, x, y) ./ _normalizationmatrix_diag(κ, x, y)
+    return kernelmatrix_diag(κ.kernel, x, y) ./
+           sqrt.(kernelmatrix_diag(κ.kernel, x) .* kernelmatrix_diag(κ.kernel, y))
 end
 
 function kernelmatrix!(
     K::AbstractMatrix, κ::NormalizedKernel, x::AbstractVector, y::AbstractVector
 )
     kernelmatrix!(K, κ.kernel, x, y)
-    K ./= _normalizationmatrix(κ, x, y)
+    K ./=
+        sqrt.(kernelmatrix_diag(κ.kernel, x) .* permutedims(kernelmatrix_diag(κ.kernel, y)))
     return K
 end
 
 function kernelmatrix!(K::AbstractMatrix, κ::NormalizedKernel, x::AbstractVector)
     kernelmatrix!(K, κ.kernel, x)
-    K ./= _normalizationmatrix(κ, x, x)
+    xdiag = kernelmatrix_diag(κ.kernel, x)
+    K ./= sqrt.(xdiag .* permutedims(xdiag))
     return K
 end
 
@@ -54,33 +61,21 @@ function kernelmatrix_diag!(
     K::AbstractVector, κ::NormalizedKernel, x::AbstractVector, y::AbstractVector
 )
     kernelmatrix_diag!(K, κ.kernel, x, y)
-    K ./= _normalizationmatrix_diag(κ, x, y)
+    K ./= sqrt.(kernelmatrix_diag(κ.kernel, x) .* kernelmatrix_diag(κ.kernel, y))
     return K
 end
 
 function kernelmatrix_diag!(K::AbstractVector, κ::NormalizedKernel, x::AbstractVector)
     kernelmatrix_diag!(K, κ.kernel, x)
-    K ./= _normalizationmatrix_diag(κ, x, x)
+    xdiag = kernelmatrix_diag(κ.kernel, x)
+    K ./= sqrt.(xdiag .* xdiag)
     return K
-end
-
-# Calculate the required normalization factor for each element
-function _normalizationmatrix(κ::NormalizedKernel, x::AbstractVector, y::AbstractVector)
-    return sqrt.(
-        kernelmatrix_diag(κ.kernel, x) .* permutedims(kernelmatrix_diag(κ.kernel, y))
-    )
-end
-
-function _normalizationmatrix_diag(
-    κ::NormalizedKernel, x::AbstractVector, y::AbstractVector
-)
-    return sqrt.(kernelmatrix_diag(κ.kernel, x) .* kernelmatrix_diag(κ.kernel, y))
 end
 
 Base.show(io::IO, κ::NormalizedKernel) = printshifted(io, κ, 0)
 
 function printshifted(io::IO, κ::NormalizedKernel, shift::Int)
-    print(io, "Normalized Kernel:" * "\n")
+    println(io, "Normalized Kernel:")
     for _ in 1:(shift + 1)
         print(io, "\t")
     end
