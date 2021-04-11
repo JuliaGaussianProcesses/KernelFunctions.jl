80b57e34f000825856789bde18efe340750669c7
David Widmann <devmotion@users.noreply.github.com>
2021-04-12 00:15:44 +0200

    Fix forat
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/deprecations.jl
+++ test/deprecations.jl
@@ -3,11 +3,10 @@
     v = rand(3)
     M = rand(3, 3)
     kernel = SqExponentialKernel()
-    
     @test (@test_deprecated transform(kernel, LinearTransform(M))) ==
-        kernel ∘ LinearTransform(M)
+          kernel ∘ LinearTransform(M)
     @test (@test_deprecated transform(kernel ∘ ScaleTransform(p), ARDTransform(v))) ==
-        kernel ∘ ARDTransform(v) ∘ ScaleTransform(p)
+          kernel ∘ ARDTransform(v) ∘ ScaleTransform(p)
     @test (@test_deprecated transform(kernel, p)) == kernel ∘ ScaleTransform(p)
     @test (@test_deprecated transform(kernel, v)) == kernel ∘ ARDTransform(v)
 end
