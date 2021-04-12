dc2989e65ea1fbb4961f20f0b901f7b7a663c41f
David Widmann <david.widmann@it.uu.se>
2021-04-13 00:17:08 +0200

    Update docstrings

--- src/kernels/transformedkernel.jl
+++ src/kernels/transformedkernel.jl
@@ -3,17 +3,11 @@
 
 Kernel derived from `k` for which inputs are transformed via a [`Transform`](@ref) `t`.
 
-It is preferred to create kernels with input transformations with [`∘`](@ref) or its
-alias `compose` instead of `TransformedKernel` directly since this allows optimized
-implementations for specific kernels and transformations.
+The preferred way to create kernels with input transformations is to use the composition
+operator [`∘`](@ref) or its alias `compose` instead of `TransformedKernel` directly since
+this allows optimized implementations for specific kernels and transformations.
 
-# Definition
-
-For inputs ``x, x'``, the transformed kernel ``\\widetilde{k}`` derived from kernel ``k`` by
-input transformation ``t`` is defined as
-```math
-\\widetilde{k}(x, x'; k, t) = k\\big(t(x), t(x')\\big).
-```
+See also: [`∘`](@ref)
 """
 struct TransformedKernel{Tk<:Kernel,Tr<:Transform} <: Kernel
     kernel::Tk
@@ -48,7 +42,26 @@ _scale(t::ScaleTransform, metric, x, y) = evaluate(metric, t(x), t(y))
 
 Compose a `kernel` with a transformation `transform` of its inputs.
 
-If no optimized implementation exists, a [`TransformedKernel`](@ref) is created.
+The prefix forms support chains of multiple transformations:
+`∘(kernel, transform1, transform2) = kernel ∘ transform1 ∘ transform2`.
+
+# Definition
+
+For inputs ``x, x'``, the transformed kernel ``\\widetilde{k}`` derived from kernel ``k`` by
+input transformation ``t`` is defined as
+```math
+\\widetilde{k}(x, x'; k, t) = k\\big(t(x), t(x')\\big).
+```
+
+# Examples
+
+```jldoctest
+julia> (SqExponentialKernel() ∘ ScaleTransform(0.5))(0, 2) == exp(-0.5)
+true
+
+julia> ∘(ExponentialKernel(), ScaleTransform(2), ScaleTransform(0.5))(1, 2) == exp(-1)
+true
+```
 
 See also: [`TransformedKernel`](@ref)
 """
