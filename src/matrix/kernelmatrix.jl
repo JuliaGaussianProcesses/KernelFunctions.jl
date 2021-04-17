24732d595bd5ff0a2a0cc4d36b5237abe32587f0
WT <wt0881@my.bristol.ac.uk>
2021-04-17 14:09:15 +0100

    Update kernelmatrix docs

--- src/matrix/kernelmatrix.jl
+++ src/matrix/kernelmatrix.jl
@@ -1,41 +1,84 @@
 """
-    kernelmatrix!(K::AbstractMatrix, κ::Kernel, X; obsdim::Integer = 2)
-    kernelmatrix!(K::AbstractMatrix, κ::Kernel, X, Y; obsdim::Integer = 2)
+    kernelmatrix!(K::AbstractMatrix, κ::Kernel, x::AbstractVector)
+    kernelmatrix!(K::AbstractMatrix, κ::Kernel, x::AbstractVector, y::AbstractVector)
 
 In-place version of [`kernelmatrix`](@ref) where pre-allocated matrix `K` will be
 overwritten with the kernel matrix.
+
+    kernelmatrix!(K::AbstractMatrix, κ::Kernel, X::AbstractMatrix; obsdim::Integer = 2)
+    kernelmatrix!(
+        K::AbstractMatrix,
+        κ::Kernel,
+        X::AbstractMatrix,
+        Y::AbstractMatrix;
+        obsdim::Integer=2,
+    )
+
+Equivalent to `kernelmatrix!(K, κ, ColVecs(X))` and
+`kernelmatrix(K, κ, ColVecs(X), ColVecs(X))` respectively.
+Set `obsdim=1` to get `RowVecs`.
+
+See also: [`ColVecs`](@ref), [`RowVecs`](@ref)
 """
 kernelmatrix!
 
 """
-    kernelmatrix(κ::Kernel, X; obsdim::Int = 2)
-    kernelmatrix(κ::Kernel, X, Y; obsdim::Int = 2)
+    kernelmatrix(κ::Kernel, x::AbstractVector)
+    kernelmatrix(κ::Kernel, x::AbstractVector, y::AbstractVector)
 
-Calculate the kernel matrix of `X` (and `Y`) with respect to kernel `κ`.
-`obsdim = 1` means the matrix `X` (and `Y`) has size #samples x #dimension
-`obsdim = 2` means the matrix `X` (and `Y`) has size #dimension x #samples
+Calculate the kernel matrix of `x` (and `y`) with respect to kernel `κ`.
+
+    kernelmatrix(κ::Kernel, X; obsdim::Int=2)
+    kernelmatrix(κ::Kernel, X, Y; obsdim::Int=2)
+
+Equivalent to `kernelmatrix(κ, ColVecs(X))` and `kernelmatrix(κ, ColVecs(X), ColVecs(Y))`
+respectively.
+Set `obsdim=1` to get `RowVecs`.
+
+See also: [`ColVecs`](@ref), [`RowVecs`](@ref)
 """
 kernelmatrix
 
 """
-    kernelmatrix_diag!(K::AbstractVector, κ::Kernel, X; obsdim::Int = 2)
-    kernelmatrix_diag!(K::AbstractVector, κ::Kernel, X, Y; obsdim::Int = 2)
+    kernelmatrix_diag!(K::AbstractVector, κ::Kernel, x::AbstractVector)
+    kernelmatrix_diag!(K::AbstractVector, κ::Kernel, x::AbstractVector, y::AbstractVector)
 
-In place version of [`kernelmatrix_diag`](@ref)
+In place version of [`kernelmatrix_diag`](@ref).
+
+    kernelmatrix_diag!(K::AbstractVector, κ::Kernel, X::AbstractMatrix; obsdim::Int=2)
+    kernelmatrix_diag!(
+        K::AbstractVector,
+        κ::Kernel,
+        X::AbstractMatrix,
+        Y::AbstractMatrix;
+        obsdim::Int=2,
+    )
+
+Equivalent to `kernelmatrix_diag!(K, κ, ColVecs(X))` and
+`kernelmatrix_diag!(K, κ, ColVecs(X), ColVecs(Y))` respectively.
+Set `obsdim=1` to get `RowVecs`.
+
+See also: [`ColVecs`](@ref), [`RowVecs`](@ref)
 """
 kernelmatrix_diag!
 
 """
-    kernelmatrix_diag(κ::Kernel, X; obsdim::Int = 2)
+    kernelmatrix_diag(κ::Kernel, x::AbstractVector)
 
-Calculate the diagonal matrix of `X` with respect to kernel `κ`
-`obsdim = 1` means the matrix `X` has size #samples x #dimension
-`obsdim = 2` means the matrix `X` has size #dimension x #samples
+Calculate the diagonal matrix of `x` with respect to kernel `κ`.
 
-    kernelmatrix_diag(κ::Kernel, X, Y; obsdim::Int = 2)
+    kernelmatrix_diag(κ::Kernel, x::AbstractVector, y::AbstractVector)
 
-Calculate the diagonal of `kernelmatrix(κ, X, Y; obsdim)` efficiently. Requires that `X` and
-`Y` are the same length.
+Calculate the diagonal of `kernelmatrix(κ, x, y)` efficiently.
+Requires that `x` and `y` are the same length.
+
+    kernelmatrix_diag(κ::Kernel, X; obsdim::Int=2)
+    kernelmatrix_diag(κ::Kernel, X, Y; obsdim::Int=2)
+
+Equivalent to `kernelmatrix_diag(κ, ColVecs(X))` and
+`kernelmatrix_diag(κ, ColVecs(X), ColVecs(Y))` respectively.
+
+See also: [`ColVecs`](@ref), [`RowVecs`](@ref)
 """
 kernelmatrix_diag
 
