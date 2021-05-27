a5fc6b20871460b5c1408c779b1275c599ba13ff
David <57544666+4aHxKzD@users.noreply.github.com>
2021-05-27 13:20:15 +0100

    Creates new methods to compute the jacobian of multioutput kernels.

--- test/finitedifferences.jl
+++ test/finitedifferences.jl
@@ -6,3 +6,26 @@ function FiniteDifferences.to_vec(x::Tuple{T, Int}) where {T}
 end
 
 FiniteDifferences.to_vec(x::Vector{Tuple{T, Int}}) where {T} = (x, identity)
+
+function FiniteDifferences._j′vp(fdm, f, ȳ::Vector{<:Real}, x::Vector{Tuple{T, Int}}) where {T}
+    isempty(x) && return eltype(ȳ)[] # if x is empty, then so is the jacobian and x̄
+    return transpose(first(jacobian(fdm, f, x))) * ȳ
+end
+
+function FiniteDifferences.jacobian(fdm, f, x::Vector{Tuple{T, Int}}; len=nothing) where {T}
+    len !== nothing && Base.depwarn(
+        "`len` keyword argument to `jacobian` is no longer required " *
+        "and will not be permitted in the future.",
+         :jacobian
+    )
+    ẏs = map(eachindex(x)) do n
+        return fdm(zero(eltype(x).types[1])) do ε
+            xn = x[n]
+            x[n] = (xn[1] + ε, xn[2])
+            ret = copy(first(to_vec(f(x))))  # copy required incase `f(x)` returns something that aliases `x`
+            x[n] = xn  # Can't do `x[n] -= ϵ` as floating-point math is not associative
+            return ret
+        end
+    end
+    return (hcat(ẏs...), )
+end
