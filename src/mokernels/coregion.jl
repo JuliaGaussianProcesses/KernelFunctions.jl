72f124eca0256d2782f9462741711f4e3c31bd99
David <57544666+4aHxKzD@users.noreply.github.com>
2021-05-28 11:48:32 +0100

    Renames kernel.

--- src/mokernels/coregion.jl
+++ src/mokernels/coregion.jl
@@ -1,5 +1,5 @@
 @doc raw"""
-    CoregionMOKernel(kernel::Kernel, B::AbstractMatrix)
+    IntrinsicIntrinsicCoregionMOKernel(kernel::Kernel, B::AbstractMatrix)
 
 Kernel associated with the intrinsic coregionalization model.
 
@@ -13,24 +13,24 @@ where ``B`` is a positive semidefinite matrix of size ``m \times m``, with ``m``
 
 [^ARL]: M. Álvarez, L. Rosasco, & N. Lawrence (2012). [Kernels for Vector-Valued Functions: a Review](https://arxiv.org/pdf/1106.6251.pdf).
 """
-struct CoregionMOKernel{K<:Kernel,T<:AbstractMatrix} <: MOKernel
+struct IntrinsicCoregionMOKernel{K<:Kernel,T<:AbstractMatrix} <: MOKernel
     kernel::K
     B::T
 
-    function CoregionMOKernel{K,T}(kernel::K, B::T) where {K,T}
-        @check_args(CoregionMOKernel, B, (eigmin(B) >= 0), "B is Positive semi-definite")
+    function IntrinsicCoregionMOKernel{K,T}(kernel::K, B::T) where {K,T}
+        @check_args(IntrinsicCoregionMOKernel, B, (eigmin(B) >= 0), "B is Positive semi-definite")
         return new{K,T}(kernel, B)
     end
 end
 
-function CoregionMOKernel(kernel::Kernel, B::AbstractMatrix)
-    return CoregionMOKernel{typeof(kernel),typeof(B)}(kernel, B)
+function IntrinsicCoregionMOKernel(kernel::Kernel, B::AbstractMatrix)
+    return IntrinsicCoregionMOKernel{typeof(kernel),typeof(B)}(kernel, B)
 end
 
-function (k::CoregionMOKernel)((x, px)::Tuple{Any,Int}, (y, py)::Tuple{Any,Int})
+function (k::IntrinsicCoregionMOKernel)((x, px)::Tuple{Any,Int}, (y, py)::Tuple{Any,Int})
     return k.B[px, py] * k.kernel(x, y)
 end
 
-function Base.show(io::IO, ::CoregionMOKernel)
-    return print(io, "Coregion Multi-Output Kernel")
+function Base.show(io::IO, ::IntrinsicCoregionMOKernel)
+    return print(io, "Intrinsic Coregion Multi-Output Kernel")
 end
