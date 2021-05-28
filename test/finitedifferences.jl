81c24768138aac0ac4031488af75bef56b25030d
David <57544666+4aHxKzD@users.noreply.github.com>
2021-05-28 11:46:46 +0100

    Reworkes to_vec so that the reimplementation of _jvp and jacobian is not needed.

--- test/finitedifferences.jl
+++ test/finitedifferences.jl
@@ -1,33 +1,7 @@
 function FiniteDifferences.to_vec(x::Tuple{T,Int}) where {T}
-    function MOinput_from_vec(x_vec)
-        return first(x_vec)
+    x_vec, first_x_from_vec = to_vec(first(x))
+    function Tuple_from_vec(x_vec::AbstractVector{<:Real})
+        return (first_x_from_vec(x_vec), last(x))
     end
-    return [x], MOinput_from_vec
-end
-
-FiniteDifferences.to_vec(x::Vector{Tuple{T,Int}}) where {T} = (x, identity)
-
-function FiniteDifferences._j′vp(
-    fdm, f, ȳ::Vector{<:Real}, x::Vector{Tuple{T,Int}}
-    ) where {T}
-    isempty(x) && return eltype(ȳ)[] # if x is empty, then so is the jacobian and x̄
-    return transpose(first(jacobian(fdm, f, x))) * ȳ
-end
-
-function FiniteDifferences.jacobian(fdm, f, x::Vector{Tuple{T,Int}}; len=nothing) where {T}
-    len !== nothing && Base.depwarn(
-        "`len` keyword argument to `jacobian` is no longer required " *
-        "and will not be permitted in the future.",
-        :jacobian
-    )
-    ẏs = map(eachindex(x)) do n
-        return fdm(zero(eltype(x).types[1])) do ε
-            xn = x[n]
-            x[n] = (xn[1] + ε, xn[2])
-            ret = copy(first(to_vec(f(x))))  # copy required incase `f(x)` returns something that aliases `x`
-            x[n] = xn  # Can't do `x[n] -= ϵ` as floating-point math is not associative
-            return ret
-        end
-    end
-    return (hcat(ẏs...),)
+    return x_vec, Tuple_from_vec
 end
