4965979324da0a6ef6fedab588252d0ee7c5764e
David <57544666+4aHxKzD@users.noreply.github.com>
2021-06-01 15:32:11 +0100

    Deletes file because we currently pass just the Real part to gradient functions when testing.

--- test/finitedifferences.jl
+++
@@ -1,7 +0,0 @@
-function FiniteDifferences.to_vec(x::Tuple{T,Int}) where {T}
-    x_vec, first_x_from_vec = to_vec(first(x))
-    function Tuple_from_vec(x_vec::AbstractVector{<:Real})
-        return (first_x_from_vec(x_vec), last(x))
-    end
-    return x_vec, Tuple_from_vec
-end
