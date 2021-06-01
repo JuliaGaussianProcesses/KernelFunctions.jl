dbd7f272518a02d5c20530ce53c712f7dcf64955
4aHxKzD <57544666+4aHxKzD@users.noreply.github.com>
2021-06-01 16:32:38 +0100

    Stops infringing character limit.
    Co-authored-by: David Widmann <devmotion@users.noreply.github.com>

--- src/mokernels/intrinsiccoregion.jl
+++ src/mokernels/intrinsiccoregion.jl
@@ -9,7 +9,9 @@ For inputs ``x, x'`` and output dimensions ``p, p'``, the kernel is defined as[^
 ```math
 k\big((x, p_x), (x', p_{x'}); B, \tilde{k}\big) = [B]_{p_x, p_{x'}} \tilde{k}\big(x, x'\big),
 ```
-where ``B`` is a positive semidefinite matrix of size ``m \times m``, with ``m`` being the number of outputs, and ``\tilde{k}`` is a scalar-valued kernel shared by the latent processes.
+where ``B`` is a positive semidefinite matrix of size ``m \times m``, with ``m`` being the
+number of outputs, and ``\tilde{k}`` is a scalar-valued kernel shared by the latent
+processes.
 
 [^ARL]: M. Álvarez, L. Rosasco, & N. Lawrence (2012). [Kernels for Vector-Valued Functions: a Review](https://arxiv.org/pdf/1106.6251.pdf).
 """
