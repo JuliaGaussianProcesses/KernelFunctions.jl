1b1a560698d30f1939d2e480ca9c8e3c73aecdf9
ST John <st--@users.noreply.github.com>
2021-06-16 13:58:39 +0300

    copy make.jl from st/examples

--- docs/make.jl
+++ docs/make.jl
@@ -1,4 +1,5 @@
 using Documenter
+using Literate
 
 # Print `@debug` statements (https://github.com/JuliaDocs/Documenter.jl/issues/955)
 if haskey(ENV, "GITHUB_ACTIONS")
@@ -6,6 +7,21 @@ if haskey(ENV, "GITHUB_ACTIONS")
 end
 
 using KernelFunctions
+
+const EXAMPLES_SRC = joinpath(@__DIR__, "..", "examples")
+const EXAMPLES_OUT = joinpath(@__DIR__, "src", "examples")
+
+if ispath(EXAMPLES_OUT)
+    rm(EXAMPLES_OUT; recursive=true)
+end
+
+for filename in readdir(EXAMPLES_SRC)
+    endswith(filename, ".jl") || continue
+    name = splitext(filename)[1]
+    Literate.markdown(
+        joinpath(EXAMPLES_SRC, filename), EXAMPLES_OUT; name=name, documenter=true
+    )
+end
 
 DocMeta.setdocmeta!(
     KernelFunctions,
@@ -32,16 +48,21 @@ makedocs(;
         "theory.md",
         "create_kernel.md",
         "API" => "api.md",
-        "Examples" => "example.md",
-        "Design" => "design.md",
+        "Examples" => [
+            "Kernel Ridge Regression" => "examples/kernel_ridge_regression.md",
+            "Training kernel parameters" => "examples/train_kernel_parameters.md",
+            "Gaussian process priors" => "examples/gaussianprocesspriors.md",
+            "SVM" => "examples/svm.md",
+            "Deep Kernel Learning" => "examples/deepkernellearning.md",
+        ],
+        "Kernel Functions" => "kernels.md",
+        "Input Transforms" => "transform.md",
+        "Metrics" => "metrics.md",
+        "Theory" => "theory.md",
+        "Custom Kernels" => "create_kernel.md",
     ],
     strict=true,
     checkdocs=:exports,
-    doctestfilters=[
-        r"{([a-zA-Z0-9]+,\s?)+[a-zA-Z0-9]+}",
-        r"(Array{[a-zA-Z0-9]+,\s?1}|Vector{[a-zA-Z0-9]+})",
-        r"(Array{[a-zA-Z0-9]+,\s?2}|Matrix{[a-zA-Z0-9]+})",
-    ],
 )
 
 deploydocs(;
