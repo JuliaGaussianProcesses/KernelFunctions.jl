b3c8aa6664e6ed82a747fa1b005441a77005c2e3
ST John <st--@users.noreply.github.com>
2021-06-23 12:37:54 +0300

    copy over fixes from KRR example

--- examples/deep-kernel-learning/script.jl
+++ examples/deep-kernel-learning/script.jl
@@ -3,6 +3,8 @@
 # !!! warning
 #     This example is under construction
 
+# Setup
+
 using KernelFunctions
 using MLDataUtils
 using Zygote
@@ -15,8 +17,13 @@ Flux.@functor KernelSum
 Flux.@functor Matern32Kernel
 Flux.@functor FunctionTransform
 
+# set up a kernel with a neural network feature extractor:
+
 neuralnet = Chain(Dense(1, 3), Dense(3, 2))
 k = SqExponentialKernel() ∘ FunctionTransform(neuralnet)
+
+# Generate date
+
 xmin = -3;
 xmax = 3;
 x = range(xmin, xmax; length=100)
@@ -24,22 +31,42 @@ x_test = rand(Uniform(xmin, xmax), 200)
 x, y = noisy_function(sinc, x; noise=0.1)
 X = reshape(x, :, 1)
 λ = [0.1]
+#md nothing #hide
+
+#
+
 function f(x, k, λ)
     return kernelmatrix(k, X, x; obsdim=1) *
            inv(kernelmatrix(k, X; obsdim=1) + exp(λ[1]) * I) *
            y
 end
 f(X, k, 1.0)
-loss(k, λ) = ŷ -> sum(y - ŷ) / length(y) + exp(λ[1]) * norm(ŷ)(f(X, k, λ))
+
+#
+
+loss(k, λ) = (ŷ -> sum(y - ŷ) / length(y) + exp(λ[1]) * norm(ŷ))(f(X, k, λ))
 loss(k, λ)
+
+#
+
 ps = Flux.params(k)
 # push!(ps,λ)
 opt = Flux.Momentum(1.0)
-##
+#md nothing #hide
+
+#
+
+plots = []
 for i in 1:10
     grads = Zygote.gradient(() -> loss(k, λ), ps)
     Flux.Optimise.update!(opt, ps, grads)
     p = Plots.scatter(x, y; lab="data", title="Loss = $(loss(k,λ))")
     Plots.plot!(x, f(X, k, λ); lab="Prediction", lw=3.0)
-    display(p)
+    push!(plots, p)
 end
+
+#
+
+l = @layout grid(10, 1)
+plot(plots...; layout=l)
+plot!(; size=(300, 1500))
