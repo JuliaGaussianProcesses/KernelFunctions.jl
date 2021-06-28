cd068bf8f43ad8cb4e7dc3185fa61957c9a2eb18
David Widmann <david.widmann@it.uu.se>
2021-06-28 15:22:33 +0200

    Run examples with separate processes

--- docs/make.jl
+++ docs/make.jl
@@ -1,3 +1,33 @@
+# run examples
+const EXAMPLES_SRC = joinpath(@__DIR__, "..", "examples")
+const EXAMPLES_OUT = joinpath(@__DIR__, "src", "examples")
+const LITERATEJL = joinpath(@__DIR__, "literate.jl")
+
+# Always rerun examples
+ispath(EXAMPLES_OUT) && rm(EXAMPLES_OUT; recursive=true)
+mkpath(EXAMPLES_OUT)
+
+# Run examples asynchronously
+processes = map(filter!(isdir, readdir(EXAMPLES_SRC; join=true))) do example
+    scriptjl = joinpath(example, "script.jl")
+    return run(
+        pipeline(
+            `$(Base.julia_cmd()) $LITERATEJL $scriptjl $EXAMPLES_OUT`;
+            stdin=devnull,
+            stdout=devnull,
+            stderr=stderr,
+        );
+        wait=false,
+    )
+end
+
+# Check that all examples were run successfully
+if !all(success, processes)
+    error("some examples were not run successfully")
+end
+
+# Build documentation
+using KernelFunctions
 using Documenter
 
 # Print `@debug` statements (https://github.com/JuliaDocs/Documenter.jl/issues/955)
@@ -5,8 +35,7 @@ if haskey(ENV, "GITHUB_ACTIONS")
     ENV["JULIA_DEBUG"] = "Documenter"
 end
 
-using KernelFunctions
-
+# Doctest setup
 DocMeta.setdocmeta!(
     KernelFunctions,
     :DocTestSetup,
@@ -31,8 +60,11 @@ makedocs(;
         "metrics.md",
         "create_kernel.md",
         "API" => "api.md",
-        "Examples" => "example.md",
         "Design" => "design.md",
+        "Examples" =>
+            map(filter!(filename -> endswith(filename, ".md"), readdir(EXAMPLES_OUT))) do x
+                return joinpath("examples", x)
+            end,
     ],
     strict=true,
     checkdocs=:exports,
