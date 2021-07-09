5ea51860c8baff20092b7b720481eb21c57629cd
Tom Wright <tom.wright@invenia.ca>
2021-07-09 15:58:46 +0100

    *Hopefully* fixing CI bug

--- test/runtests.jl
+++ test/runtests.jl
@@ -181,8 +181,8 @@ include("test_utils.jl")
                 KernelFunctions;
                 doctestfilters=[
                     r"{([a-zA-Z0-9]+,\s?)+[a-zA-Z0-9]+}",
-                    r"(Array{[a-zA-Z0-9]+,\s?1}|Vector{[a-zA-Z0-9]+})",
-                    r"(Array{[a-zA-Z0-9]+,\s?2}|Matrix{[a-zA-Z0-9]+})",
+                    r"(Array{[a-zA-Z0-9]+,\s?1}|\s?Vector{[a-zA-Z0-9]+})",
+                    r"(Array{[a-zA-Z0-9]+,\s?2}|\s?Matrix{[a-zA-Z0-9]+})",
                 ],
             )
         end
