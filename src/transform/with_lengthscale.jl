f6c5d28bf4ae95a79ea9035e224e516a7128585b
ST John <st--@users.noreply.github.com>
2021-07-09 15:32:01 +0300

    imperative

--- src/transform/with_lengthscale.jl
+++ src/transform/with_lengthscale.jl
@@ -2,8 +2,8 @@
     with_lengthscale(kernel::Kernel, lengthscale::Real)
     with_lengthscale(kernel::Kernel, lengthscales::AbstractVector{<:Real})
 
-Constructs a transformed kernel with `lengthscale`.
-If a vector `lengthscales` is passed instead, constructs an "ARD" kernel with different lengthscales for each dimension.
+Construct a transformed kernel with `lengthscale`.
+If a vector `lengthscales` is passed instead, construct an "ARD" kernel with different lengthscales for each dimension.
 
 The following two ways of constructing a squared-exponential kernel with
 a given lengthscale are equivalent:
