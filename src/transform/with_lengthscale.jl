1ae1b269dc3cc733f2fc7d77b7508f15d4a4734d
ST John <st--@users.noreply.github.com>
2021-07-09 14:09:39 +0300

    with_lengthscale

---
+++ src/transform/with_lengthscale.jl
@@ -0,0 +1,34 @@
+"""
+    with_lengthscale(kernel::Kernel, lengthscale::Real)
+    with_lengthscale(kernel::Kernel, lengthscales::AbstractVector{<:Real})
+
+Constructs a transformed kernel with `lengthscale`. If `lengthscales` is passed
+as a vector instead, constructs an "ARD" kernel with different lengthscales for
+each dimension.
+
+The following two ways of constructing a squared-exponential kernel with
+a given lengthscale are equivalent:
+
+```jldoctest
+julia> ℓ = 2.5;
+
+julia> SqExponentialKernel() ∘ ScaleTransform(inv(ℓ)) == with_lengthscale(SqExponentialKernel(), ℓ)
+true
+```
+
+and for the ARD case:
+
+```jldoctest
+julia> ℓ = [0.5, 2.5];
+
+julia> SqExponentialKernel() ∘ ARDTransform(inv.(ℓ)) == with_lengthscale(SqExponentialKernel(), ℓ)
+true
+```
+
+"""
+function with_lengthscale(kernel::Kernel, lengthscale::Real)
+    return compose(kernel, ScaleTransform(inv(lengthscale)))
+end
+function with_lengthscale(kernel::Kernel, lengthscales::AbstractVector{<:Real})
+    return compose(kernel, ARDTransform(inv.(lengthscales)))
+end
