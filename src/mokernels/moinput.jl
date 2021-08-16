dadfbb31ecd8f371a836c7ed8663287595cadce7
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2021-08-16 17:33:55 +0100

    Some improvement and additions to MO kernels (#354)
    * Fix type stability of independent kernel
    
    * Add convenience functions
    
    * Add/fix tests
    
    * Suggested change for kernelmatrix computation
    
    * Add results to test script
    
    * Switching to new kernelmatrix for independent kernel, tests
    
    * Change type stability
    
    * New kernelmatrix for IntrinsicCoregion kernel, tests
    
    * JuliaFormatter
    
    * Improve code reuse, Traits
    
    * Use FillArray
    
    * Remove traits, move to explicit function
    
    * Relax matrixkernel types
    
    Co-authored-by: st-- <st--@users.noreply.github.com>
    
    * Adjust MOInput specifications
    
    * Change supertype, move matrixkernel
    
    * JuliaFormatter
    
    * Remove forced typesymmetry, and improve fallback
    
    * Improve documentation, add matrixkernel to other kernels
    
    * Comment out specialized kernelmatrix!
    
    * Add kernelmatrix! back in with version check
    
    * Formatter
    
    * Improve doc phrasing
    
    Co-authored-by: st-- <st--@users.noreply.github.com>
    
    * Change Union name
    
    * Substantially improve tests
    
    * Add ed clarity for lazy kronecker
    
    * Improve tests for lmm and slfm
    
    * Formatter
    
    * Update src/mokernels/mokernel.jl
    
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>
    
    * Remove comments
    
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>
    
    * Add check for lmm kernel, tests
    
    * Fix constructor mistake
    
    * Remove kwarg dispatch
    
    * Remove matrixkernel and lazy kron
    
    * Remove matrixkernel export
    
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>
    
    * Update src/matrix/kernelkroneckermat.jl
    
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>
    
    * Update src/mokernels/independent.jl
    
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>
    
    * Update src/mokernels/independent.jl
    
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>
    
    * Update src/mokernels/mokernel.jl
    
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>
    
    * helper function name, formatter
    
    * Remove explicit in-place tests
    
    * Remove temp file and bump patch
    
    * Change helper function name
    
    * Fixed rename oversight
    
    * Fix missing Union
    
    * Silly mistake
    
    * Make arguments more consistent
    
    * Forgot to uncomment
    
    Co-authored-by: st-- <st--@users.noreply.github.com>
    Co-authored-by: willtebbutt <wt0881@my.bristol.ac.uk>

--- src/mokernels/moinput.jl
+++ src/mokernels/moinput.jl
@@ -58,7 +58,7 @@ struct MOInputIsotopicByOutputs{S,T<:AbstractVector{S}} <: AbstractVector{Tuple{
     out_dim::Integer
 end
 
-const IsotopicMOInputs = Union{MOInputIsotopicByFeatures,MOInputIsotopicByOutputs}
+const IsotopicMOInputsUnion = Union{MOInputIsotopicByFeatures,MOInputIsotopicByOutputs}
 
 function Base.getindex(inp::MOInputIsotopicByOutputs, ind::Integer)
     @boundscheck checkbounds(inp, ind)
@@ -74,7 +74,7 @@ function Base.getindex(inp::MOInputIsotopicByFeatures, ind::Integer)
     return feature, output_index
 end
 
-Base.size(inp::IsotopicMOInputs) = (inp.out_dim * length(inp.x),)
+Base.size(inp::IsotopicMOInputsUnion) = (inp.out_dim * length(inp.x),)
 
 function Base.vcat(x::MOInputIsotopicByFeatures, y::MOInputIsotopicByFeatures)
     x.out_dim == y.out_dim || throw(DimensionMismatch("out_dim mismatch"))
--- test/mokernels/slfm.jl
+++ test/mokernels/slfm.jl
@@ -1,11 +1,20 @@
 @testset "slfm" begin
     rng = MersenneTwister(123)
     FDM = FiniteDifferences.central_fdm(5, 1)
-    N = 10
-    in_dim = 5
+    N = 6
+    in_dim = 3
     out_dim = 4
-    x1 = MOInput([rand(rng, in_dim) for _ in 1:N], out_dim)
-    x2 = MOInput([rand(rng, in_dim) for _ in 1:N], out_dim)
+    x1IO = KernelFunctions.MOInputIsotopicByOutputs(
+        [rand(rng, in_dim) for _ in 1:N], out_dim
+    )
+    x2IO = KernelFunctions.MOInputIsotopicByOutputs(
+        [rand(rng, in_dim) for _ in 1:N], out_dim
+    )
+    x3IO = KernelFunctions.MOInputIsotopicByOutputs(
+        [rand(rng, in_dim) for _ in 1:div(N, 2)], out_dim
+    )
+    x1IO = MOInput([rand(rng, in_dim) for _ in 1:N], out_dim)
+    x2IO = MOInput([rand(rng, in_dim) for _ in 1:N], out_dim)
 
     k = LatentFactorMOKernel(
         [Matern32Kernel(), SqExponentialKernel(), FBMKernel()],
@@ -15,10 +24,17 @@
     @test k isa LatentFactorMOKernel
     @test k isa MOKernel
     @test k isa Kernel
-    @test k(x1[1], x2[1]) isa Real
+    @test k(x1IO[1], x2IO[1]) isa Real
 
-    @test kernelmatrix(k, x1, x2) ≈ kernelmatrix(k, collect(x1), collect(x2))
-    @test kernelmatrix(k, x1, x1) ≈ kernelmatrix(k, x1)
+    @test kernelmatrix(k, x1IO, x2IO) ≈ kernelmatrix(k, collect(x1IO), collect(x2IO))
+
+    TestUtils.test_interface(k, x1IO, x2IO, x3IO)
+
+    x1IF = KernelFunctions.MOInputIsotopicByFeatures(x1IO.x, out_dim)
+    x2IF = KernelFunctions.MOInputIsotopicByFeatures(x2IO.x, out_dim)
+    x3IF = KernelFunctions.MOInputIsotopicByFeatures(x3IO.x, out_dim)
+
+    TestUtils.test_interface(k, x1IF, x2IF, x3IF)
 
     @test string(k) == "Semi-parametric Latent Factor Multi-Output Kernel"
     @test repr("text/plain", k) == (
@@ -31,13 +47,13 @@
     )
 
     # AD test
-    function test_slfm(A::AbstractMatrix, x1, x2)
+    function test_slfm(A::AbstractMatrix, x1IO, x2IO)
         k = LatentFactorMOKernel(
             [Matern32Kernel(), SqExponentialKernel(), FBMKernel()],
             IndependentMOKernel(GaussianKernel()),
             A,
         )
-        return k((x1, 1), (x2, 1))
+        return k((x1IO, 1), (x2IO, 1))
     end
 
     k = LatentFactorMOKernel(
