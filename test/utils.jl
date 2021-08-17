92821dd69b6319c248014ad417a6cca6696e34c2
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2021-08-17 09:49:44 +0100

    Qualify pairwise call

--- test/utils.jl
+++ test/utils.jl
@@ -98,12 +98,38 @@
         x_rowvecs = RowVecs(randn(7, 3))
 
         @test isapprox(
-            pairwise(SqEuclidean(), x_colvecs, x_rowvecs),
-            pairwise(SqEuclidean(), collect(x_colvecs), collect(x_rowvecs)),
+            KernelFunctions.pairwise(SqEuclidean(), x_colvecs, x_rowvecs),
+            KernelFunctions.pairwise(SqEuclidean(), collect(x_colvecs), collect(x_rowvecs)),
         )
         @test isapprox(
             pairwise(SqEuclidean(), x_rowvecs, x_colvecs),
             pairwise(SqEuclidean(), collect(x_rowvecs), collect(x_colvecs)),
+        )
+    end
+    @testset "AbstractVector + RowVecs" begin
+        x = [randn(3) for _ in 1:5]
+        x_rowvecs = RowVecs(randn(7, 3))
+    
+        @test isapprox(
+            KernelFunctions.pairwise(SqEuclidean(), x, x_rowvecs),
+            pairwise(SqEuclidean(), x, collect(x_rowvecs)),
+        )
+        @test isapprox(
+            KernelFunctions.pairwise(SqEuclidean(), x_rowvecs, x),
+            pairwise(SqEuclidean(), collect(x_rowvecs), x),
+        )
+    end
+    @testset "AbstractVector + ColVecs" begin
+        x = [randn(3) for _ in 1:5]
+        x_colvecs = ColVecs(randn(3, 7))
+    
+        @test isapprox(
+            KernelFunctions.pairwise(SqEuclidean(), x, x_colvecs),
+            pairwise(SqEuclidean(), x, collect(x_colvecs)),
+        )
+        @test isapprox(
+            KernelFunctions.pairwise(SqEuclidean(), x_colvecs, x),
+            pairwise(SqEuclidean(), collect(x_colvecs), x),
         )
     end
     @testset "input checks" begin
