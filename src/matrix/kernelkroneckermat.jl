c76b27d178d767c304fbf3bcb105252d130ef49f
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2021-09-23 13:42:21 +0100

    Add lazy kronecker product for matrix kernels, if Kronecker.jl is loaded (#364)
    * Restore additions
    
    * Improvements for lazy kron
    
    * Remove unneeded lines
    
    * Small experiment with overwriting
    
    * Reorder and overwrite
    
    * Format and kernelmatrix!
    
    * Reinstate separate method
    
    * Adding tests
    
    * Duplicate code for readability
    
    * Format
    
    * Remove comment and patch bump
    
    * Change kernelmatrix!
    
    * Change to output covariance type
    
    * Change to output covariance type - revert
    
    * Revert "Change to output covariance type - revert"
    
    This reverts commit 09cd20e2d461d67d52766cb01ce59be248839660.
    
    * Revert "Change kernelmatrix!"
    
    This reverts commit f46bd61130165b0b0b47ca0fb8aea277ddc20d55.
    
    * Add kernelmatrix! changes again
    
    * Change input types for pairwise pullback
    
    * Missing changes to Any

--- src/matrix/kernelkroneckermat.jl
+++ src/matrix/kernelkroneckermat.jl
@@ -4,6 +4,7 @@
 using .Kronecker: Kronecker
 
 export kernelkronmat
+export kronecker_kernelmatrix
 
 function kernelkronmat(κ::Kernel, X::AbstractVector, dims::Int)
     @assert iskroncompatible(κ) "The chosen kernel is not compatible for kroenecker matrices (see [`iskroncompatible`](@ref))"
@@ -25,3 +26,42 @@ end
     k(x,x') = ∏ᵢᴰ k(xᵢ,x'ᵢ)
 """
 @inline iskroncompatible(κ::Kernel) = false # Default return for kernels
+
+function _kernelmatrix_kroneckerjl_helper(::MOInputIsotopicByFeatures, Kfeatures, Koutputs)
+    return Kronecker.kronecker(Kfeatures, Koutputs)
+end
+
+function _kernelmatrix_kroneckerjl_helper(::MOInputIsotopicByOutputs, Kfeatures, Koutputs)
+    return Kronecker.kronecker(Koutputs, Kfeatures)
+end
+
+function kronecker_kernelmatrix(
+    k::Union{IndependentMOKernel,IntrinsicCoregionMOKernel},
+    x::IsotopicMOInputsUnion,
+    y::IsotopicMOInputsUnion,
+)
+    @assert x.out_dim == y.out_dim
+    Kfeatures = kernelmatrix(k.kernel, x.x, y.x)
+    Koutputs = _mo_output_covariance(k, x.out_dim)
+    return _kernelmatrix_kroneckerjl_helper(x, Kfeatures, Koutputs)
+end
+
+function kronecker_kernelmatrix(
+    k::Union{IndependentMOKernel,IntrinsicCoregionMOKernel}, x::IsotopicMOInputsUnion
+)
+    Kfeatures = kernelmatrix(k.kernel, x.x)
+    Koutputs = _mo_output_covariance(k, x.out_dim)
+    return _kernelmatrix_kroneckerjl_helper(x, Kfeatures, Koutputs)
+end
+
+function kronecker_kernelmatrix(
+    k::MOKernel, x::IsotopicMOInputsUnion, y::IsotopicMOInputsUnion
+)
+    return throw(
+        ArgumentError("This kernel does not support a lazy kronecker kernelmatrix.")
+    )
+end
+
+function kronecker_kernelmatrix(k::MOKernel, x::IsotopicMOInputsUnion)
+    return kronecker_kernelmatrix(k, x, x)
+end
--- src/mokernels/mokernel.jl
+++ src/mokernels/mokernel.jl
@@ -4,3 +4,21 @@
 Abstract type for kernels with multiple outpus.
 """
 abstract type MOKernel <: Kernel end
+
+function _kernelmatrix_kron_helper(::MOInputIsotopicByFeatures, Kfeatures, Koutputs)
+    return kron(Kfeatures, Koutputs)
+end
+
+function _kernelmatrix_kron_helper(::MOInputIsotopicByOutputs, Kfeatures, Koutputs)
+    return kron(Koutputs, Kfeatures)
+end
+
+if VERSION >= v"1.6"
+    function _kernelmatrix_kron_helper!(K, ::MOInputIsotopicByFeatures, Kfeatures, Koutputs)
+        return kron!(K, Kfeatures, Koutputs)
+    end
+
+    function _kernelmatrix_kron_helper!(K, ::MOInputIsotopicByOutputs, Kfeatures, Koutputs)
+        return kron!(K, Koutputs, Kfeatures)
+    end
+end
