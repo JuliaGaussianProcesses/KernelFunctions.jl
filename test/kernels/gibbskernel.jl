4ddafbebc4c67ad49ca3205217505c5c8fbdf14a
Sebastian Khan <KhanS22@Cardiff.ac.uk>
2021-09-30 11:46:11 +0100

    ensure ell returns positive value in test
    added a broken test to seek help about why it's broken

--- test/kernels/gibbskernel.jl
+++ test/kernels/gibbskernel.jl
@@ -3,13 +3,14 @@
     y = randn(2)
 
     # generate random number of standard SqExponentialKernel lengthscale
-    ell = sum(sin, x)
+    # taking exp() to ensure ell is positive
+    ell(x) = exp(sum(sin, x))
 
     # this is the gibbs lengthscale function.
     # to check that we can recover the stationary SqExponentialKernel
     # with a constant lengthscale we just set this function
     # equal to a constant.
-    l_func(x) = ell
+    l_func(x) = ell(1)
 
     # in order to compare to the Gibbs kernel we compute the
     # equivalent lengthscale l(x)^2 + l(y)^2.
@@ -24,4 +25,8 @@
 
     # check they are equal
     @test k_gibbs(x, y) == k(x, y)
+
+    k_gibbs = GibbsKernel(ell)
+    @test k_gibbs(x, y) ≈ sqrt((2 * ell(x) * ell(y)) / (ell(x)^2 + ell(y)^2)) * exp(- norm(x - y)^2 / (ell(x)^2 + ell(y)^2))
+
 end
