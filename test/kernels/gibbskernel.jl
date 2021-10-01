79aa4f599648b1436ac1db686670a2dd551bd4a0
Sebastian Khan <KhanS22@Cardiff.ac.uk>
2021-10-01 12:53:13 +0100

    gibbskernel test: formatting

--- test/kernels/gibbskernel.jl
+++ test/kernels/gibbskernel.jl
@@ -7,6 +7,8 @@
     # create a gibbs kernel with our specific lengthscale function
     k_gibbs = GibbsKernel(ell)
 
-    @test k_gibbs(x, y) ≈ sqrt((2 * ell(x) * ell(y)) / (ell(x)^2 + ell(y)^2)) * exp(-(x - y)^2 / (ell(x)^2 + ell(y)^2))
+    @test k_gibbs(x, y) ≈
+          sqrt((2 * ell(x) * ell(y)) / (ell(x)^2 + ell(y)^2)) *
+          exp(-(x - y)^2 / (ell(x)^2 + ell(y)^2))
 
 end
