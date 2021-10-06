21372f01262ac264bb5d0a6ca25c9f7803538bc4
Sebastian Khan <KhanS22@Cardiff.ac.uk>
2021-10-06 11:16:31 +0100

    improve code formatting

--- examples/gaussian-process-priors/script.jl
+++ examples/gaussian-process-priors/script.jl
@@ -120,7 +120,7 @@ kernels = [
     LinearKernel(),
     compose(PeriodicKernel(), ScaleTransform(0.2)),
     NeuralNetworkKernel(),
-    GibbsKernel(; lengthscale = x -> sum(exp ∘ sin, x)),
+    GibbsKernel(; lengthscale=x -> sum(exp ∘ sin, x)),
 ]
 plot(
     [visualize(k) for k in kernels]...;
