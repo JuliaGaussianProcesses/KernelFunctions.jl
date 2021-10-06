91cc6c0d5ac8f5864bc0418c740aa23656671ff0
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2021-10-06 14:07:09 +0100

    Potential fix for Delta distance

--- src/distances/delta.jl
+++ src/distances/delta.jl
@@ -1,19 +1,20 @@
 # Delta is not following the PreMetric rules since d(x, x) == 1
 struct Delta <: Distances.UnionPreMetric end
 
-@inline function Distances._evaluate(::Delta, a::AbstractVector, b::AbstractVector)
-    @boundscheck if length(a) != length(b)
-        throw(
-            DimensionMismatch(
-                "first array has length $(length(a)) which does not match the length of the " *
-                "second, $(length(b)).",
-            ),
-        )
-    end
-    return a == b
-end
+# @inline function Distances._evaluate(::Delta, a::AbstractVector, b::AbstractVector)
+#     @boundscheck if length(a) != length(b)
+#         throw(
+#             DimensionMismatch(
+#                 "first array has length $(length(a)) which does not match the length of the " *
+#                 "second, $(length(b)).",
+#             ),
+#         )
+#     end
+#     return a == b
+# end
 
-Distances.result_type(::Delta, Ta::Type, Tb::Type) = Bool
-
+@inline Distances.eval_op(::Sinus, a::Real, b::Real) = a == b
 @inline (dist::Delta)(a::AbstractArray, b::AbstractArray) = Distances._evaluate(dist, a, b)
 @inline (dist::Delta)(a::Number, b::Number) = a == b
+
+Distances.result_type(::Delta, Ta::Type, Tb::Type) = Bool
