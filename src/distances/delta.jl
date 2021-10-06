9660692622423c391c167fe5620d3fee131577bc
Steffen Ridderbusch <steffen@robots.ox.ac.uk>
2021-10-06 15:13:14 +0100

    Remove old Delta implementation

--- src/distances/delta.jl
+++ src/distances/delta.jl
@@ -1,18 +1,6 @@
 # Delta is not following the PreMetric rules since d(x, x) == 1
 struct Delta <: Distances.UnionPreMetric end
 
-# @inline function Distances._evaluate(::Delta, a::AbstractVector, b::AbstractVector)
-#     @boundscheck if length(a) != length(b)
-#         throw(
-#             DimensionMismatch(
-#                 "first array has length $(length(a)) which does not match the length of the " *
-#                 "second, $(length(b)).",
-#             ),
-#         )
-#     end
-#     return a == b
-# end
-
 @inline Distances.eval_op(::Delta, a::Real, b::Real) = a == b
 @inline Distances.eval_reduce(::Delta, a, b) = a && b
 @inline Distances.eval_start(::Delta, a, b) = true
