f78f02830bd7ba544035dc3bdfa063244fe1f3e6
David Widmann <devmotion@users.noreply.github.com>
2021-10-08 16:07:20 +0200

    Simplify implementation of `Delta` (#378)
    * Simplify implementation of `Delta`
    
    * Fix bug in `pairwise!` implementation
    
    * Update Project.toml

--- src/distances/delta.jl
+++ src/distances/delta.jl
@@ -1,10 +1,18 @@
 # Delta is not following the PreMetric rules since d(x, x) == 1
 struct Delta <: Distances.UnionPreMetric end
 
-@inline Distances.eval_op(::Delta, a::Real, b::Real) = a == b
-@inline Distances.eval_reduce(::Delta, a, b) = a && b
-@inline Distances.eval_start(::Delta, a, b) = true
-@inline (dist::Delta)(a::AbstractArray, b::AbstractArray) = Distances._evaluate(dist, a, b)
-@inline (dist::Delta)(a::Number, b::Number) = a == b
+(dist::Delta)(a::Number, b::Number) = a == b
+Base.@propagate_inbounds function (dist::Delta)(
+    a::AbstractArray{<:Number}, b::AbstractArray{<:Number}
+)
+    @boundscheck if length(a) != length(b)
+        throw(
+            DimensionMismatch(
+                "first array has length $(length(a)) which does not match the length of the second, $(length(b)).",
+            ),
+        )
+    end
+    return a == b
+end
 
 Distances.result_type(::Delta, Ta::Type, Tb::Type) = Bool
