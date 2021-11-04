f4cd28a6290caaa243ab84329f64eb3ea61fb910
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2021-11-04 14:00:35 +0100

    Apply suggestions from code review
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -17,21 +17,16 @@ Yv = collect.(eachcol(Y))
 SUITE = BenchmarkGroup()
 
 kernels = Dict(
-    "SqExponential" => SqExponentialKernel(),
-    "Exponential" => ExponentialKernel(),
+    "SqExponential" => SqExponentialKernel(), "Exponential" => ExponentialKernel()
 )
 
-inputtypes = Dict(
-    "ColVecs" => (Xc, Yc),
-    "RowVecs" => (Xr, Yr),
-    "Vecs" => (Xv, Yv),
-)
+inputtypes = Dict("ColVecs" => (Xc, Yc), "RowVecs" => (Xr, Yr), "Vecs" => (Xv, Yv))
 
 functions = Dict(
     "kernelmatrixX" => (kernel, X, Y) -> kernelmatrix(kernel, X),
     "kernelmatrixXY" => (kernel, X, Y) -> kernelmatrix(kernel, X, Y),
     "kernelmatrix_diagX" => (kernel, X, Y) -> kernelmatrix_diag(kernel, X),
-    "kernelmatrix_diagXY" => (kernel, X, Y) -> kernelmatrix_diag(kernel, X, Y)
+    "kernelmatrix_diagXY" => (kernel, X, Y) -> kernelmatrix_diag(kernel, X, Y),
 )
 
 for (kname, kernel) in kernels
