1c71067d3fedde292a1b3228cf062ce7eaa3bcb0
ST John <st--@users.noreply.github.com>
2022-01-11 18:05:51 +0200

    bugfix & improve writing, replaces #321

--- examples/support-vector-machine/script.jl
+++ examples/support-vector-machine/script.jl
@@ -2,7 +2,7 @@
 #
 # In this notebook we show how you can use KernelFunctions.jl to generate
 # kernel matrices for classification with a support vector machine, as
-# implemented by LIBSVM.
+# implemented by [LIBSVM](https://github.com/JuliaML/LIBSVM.jl).
 
 using Distributions
 using KernelFunctions
@@ -27,28 +27,28 @@ X1 = [cos.(angle1) sin.(angle1)] .+ 0.1randn(n1, 2)
 X2 = [1 .- cos.(angle2) 1 .- sin.(angle2) .- 0.5] .+ 0.1randn(n2, 2)
 X = [X1; X2]
 x_train = RowVecs(X)
-y_train = vcat(fill(-1, nout), fill(1, nin));
+y_train = vcat(fill(-1, n1), fill(1, n2));
 
-# Create a 100×100 2D grid for evaluation:
+# ## Training
+#
+# We create a kernel function:
+k = SqExponentialKernel() ∘ ScaleTransform(1.5)
+
+# LIBSVM can make use of a pre-computed kernel matrix.
+# KernelFunctions.jl can be used to produce that using `kernelmatrix`:
+model = svmtrain(kernelmatrix(k, x_train), y_train; kernel=LIBSVM.Kernel.Precomputed)
+
+# ## Prediction
+#
+# For evaluation, we create a 100×100 2D grid based on the extent of the training data:
 test_range = range(floor(Int, minimum(X)), ceil(Int, maximum(X)); length=100)
 x_test = ColVecs(mapreduce(collect, hcat, Iterators.product(test_range, test_range)));
 
-# ## SVM model
-#
-# Create kernel function:
-k = SqExponentialKernel() ∘ ScaleTransform(1.5)
-
-# [LIBSVM](https://github.com/JuliaML/LIBSVM.jl) can make use of a pre-computed kernel matrix.
-# KernelFunctions.jl can be used to produce that.
-#
-# Precomputed matrix for training
-model = svmtrain(kernelmatrix(k, x_train), y_train; kernel=LIBSVM.Kernel.Precomputed)
-
-# Precomputed matrix for prediction
+# Again, we pass the result of KernelFunctions.jl's `kernelmatrix` to LIBSVM:
 y_pred, _ = svmpredict(model, kernelmatrix(k, x_train, x_test));
 
-# Visualize prediction on a grid:
+# We can see that the kernelized, non-linear classification successfully separates the two classes in the training data:
 plot(; xlim=extrema(test_range), ylim=extrema(test_range), aspect_ratio=1)
-contourf!(test_range, test_range, y_pred; levels=1, color=cgrad(:redsblues), alpha=0.7)
-scatter!(X1[:, 1], X1[:, 2]; color=:red, label="class 1")
-scatter!(X2[:, 1], X2[:, 2]; color=:blue, label="class 2")
+contourf!(test_range, test_range, y_pred; levels=1, color=cgrad(:redsblues), alpha=0.7, colorbar_title="prediction")
+scatter!(X1[:, 1], X1[:, 2]; color=:red, label="training data: class –1")
+scatter!(X2[:, 1], X2[:, 2]; color=:blue, label="training data: class 1")
