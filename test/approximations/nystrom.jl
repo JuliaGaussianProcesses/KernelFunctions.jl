e85a977edc458013bbd3529c018a01e65d65a01f
st-- <st--@users.noreply.github.com>
2022-01-21 11:01:53 +0100

    Update test/approximations/nystrom.jl
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- test/approximations/nystrom.jl
+++ test/approximations/nystrom.jl
@@ -12,6 +12,6 @@
         @test kernelmatrix(k, X; obsdim=obsdim) ≈
             kernelmatrix(nystrom(k, X, 1.0; obsdim=obsdim))
         @test kernelmatrix(k, X; obsdim=obsdim) ≈
-              kernelmatrix(nystrom(k, X, collect(1:dims[obsdim]); obsdim=obsdim))
+            kernelmatrix(nystrom(k, X, collect(1:dims[obsdim]); obsdim=obsdim))
     end
 end
