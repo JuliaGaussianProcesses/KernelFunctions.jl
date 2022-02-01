eb093963b53237a28bd71863af4eb0c3d0ebbf6e
WT <wt0881@my.bristol.ac.uk>
2022-02-01 11:36:27 +0000

    Use Fill

--- src/basekernels/constant.jl
+++ src/basekernels/constant.jl
@@ -78,11 +78,11 @@ kappa(κ::ConstantKernel, x::Real) = only(κ.c) * one(x)
 metric(::ConstantKernel) = Delta()
 
 function kernelmatrix(k::ConstantKernel, x::AbstractVector)
-    return fill(only(k.c), length(x), length(x))
+    return Fill(only(k.c), length(x), length(x))
 end
 
 function kernelmatrix(k::ConstantKernel, x::AbstractVector, y::AbstractVector)
-    return fill(only(k.c), length(x), length(y))
+    return Fill(only(k.c), length(x), length(y))
 end
 
 
