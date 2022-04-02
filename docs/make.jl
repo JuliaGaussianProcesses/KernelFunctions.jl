01c3f5c87adb1c9d9e21d87352e13f411e622916
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-04-02 17:42:13 +0200

    Adapt to JuliaGPsDocs

--- docs/make.jl
+++ docs/make.jl
@@ -1,45 +1,15 @@
 ### Process examples
-# Always rerun examples
-const EXAMPLES_OUT = joinpath(@__DIR__, "src", "examples")
-ispath(EXAMPLES_OUT) && rm(EXAMPLES_OUT; recursive=true)
-mkpath(EXAMPLES_OUT)
-
-# Install and precompile all packages
-# Workaround for https://github.com/JuliaLang/Pkg.jl/issues/2219
-examples = filter!(isdir, readdir(joinpath(@__DIR__, "..", "examples"); join=true))
-let script = "using Pkg; Pkg.activate(ARGS[1]); Pkg.instantiate()"
-    for example in examples
-        if !success(`$(Base.julia_cmd()) -e $script $example`)
-            error(
-                "project environment of example ",
-                basename(example),
-                " could not be instantiated",
-            )
-        end
-    end
-end
-# Run examples asynchronously
-processes = let literatejl = joinpath(@__DIR__, "literate.jl")
-    map(examples) do example
-        return run(
-            pipeline(
-                `$(Base.julia_cmd()) $literatejl $(basename(example)) $EXAMPLES_OUT`;
-                stdin=devnull,
-                stdout=devnull,
-                stderr=stderr,
-            );
-            wait=false,
-        )::Base.Process
-    end
-end
-
-# Check that all examples were run successfully
-isempty(processes) || success(processes) || error("some examples were not run successfully")
+using Pkg
+Pkg.add(Pkg.PackageSpec(; url="https://github.com/JuliaGaussianProcesses/JuliaGPsDocs.jl")) # While the package is unregistered, it's a workaround
 
 ### Build documentation
 using Documenter
 
+using JuliaGPsDocs
 using KernelFunctions
+
+JuliaGPsDocs.generate_examples(ApproximateGPs)
+
 using PDMats, Kronecker  # we have to load all optional packages to generate the full API documentation
 
 # Doctest setup
@@ -65,10 +35,13 @@ makedocs(;
         "create_kernel.md",
         "API" => "api.md",
         "Design" => "design.md",
-        "Examples" =>
-            map(filter!(filename -> endswith(filename, ".md"), readdir(EXAMPLES_OUT))) do x
-                return joinpath("examples", x)
-            end,
+        "Examples" => map(
+            basename.(
+                filter!(isdir, readdir(joinpath(@__DIR__, "src", "examples"); join=true)),
+            ),
+        ) do x
+            joinpath("examples", x, "index.md")
+        end,
     ],
     strict=true,
     checkdocs=:exports,
