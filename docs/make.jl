289a1400f6a404d12bfcaf8075296dc9934eecc0
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2022-04-08 15:02:34 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -39,11 +39,7 @@ makedocs(;
     ],
     strict=true,
     checkdocs=:exports,
-    doctestfilters=[
-        r"{([a-zA-Z0-9]+,\s?)+[a-zA-Z0-9]+}",
-        r"(Array{[a-zA-Z0-9]+,\s?1}|Vector{[a-zA-Z0-9]+})",
-        r"(Array{[a-zA-Z0-9]+,\s?2}|Matrix{[a-zA-Z0-9]+})",
-    ],
+    doctestfilters=JuliaGPsDocs.DOCTEST_FILTERS,
 )
 
 deploydocs(;
