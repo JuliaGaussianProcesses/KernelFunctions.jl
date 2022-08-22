617be1c0399f9eaa728c846a750b05c60716a697
Will Tebbutt <will.tebbutt@invenialabs.co.uk>
2022-08-22 16:10:57 +0100

    Uncomment tests

--- test/basekernels/gabor.jl
+++ test/basekernels/gabor.jl
@@ -25,13 +25,13 @@
 
     @test gaborkernel() isa KernelProduct{<:Tuple{<:SqExponentialKernel,<:CosineKernel}}
 
-    # test_ADs(
-    #     x -> gaborkernel(;
-    #         sqexponential_transform=ScaleTransform(x[1]),
-    #         cosine_transform=ScaleTransform(x[2]),
-    #     ),
-    #     [ell, p],
-    # )
+    test_ADs(
+        x -> gaborkernel(;
+            sqexponential_transform=ScaleTransform(x[1]),
+            cosine_transform=ScaleTransform(x[2]),
+        ),
+        [ell, p],
+    )
     test_interface_ad_perf((ell, p), StableRNG(123456)) do θ
         gaborkernel(;
             sqexponential_transform=ScaleTransform(θ[1]),
