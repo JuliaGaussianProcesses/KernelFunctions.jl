c84eed88baaae0b56816fef3728077b08a9857e0
Will Tebbutt <will.tebbutt@invenialabs.co.uk>
2022-08-22 15:42:00 +0100

    Enable all nn tests

--- test/basekernels/nn.jl
+++ test/basekernels/nn.jl
@@ -8,5 +8,5 @@
     # Standardised tests.
     TestUtils.test_interface(k, Float64)
     # test_ADs(NeuralNetworkKernel)
-    test_interface_ad_perf(_ -> NeuralNetworkKernel(), nothing, StableRNG(123456), [Vector{Float64}])
+    test_interface_ad_perf(_ -> NeuralNetworkKernel(), nothing, StableRNG(123456))
 end
