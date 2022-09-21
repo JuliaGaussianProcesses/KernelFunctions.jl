e393421923bcae62fbcb28dfe91389f82f2acc9d
Will Tebbutt <wct23@cam.ac.uk>
2022-09-21 10:49:45 +0100

    Formatting
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>

--- src/basekernels/linear_spline.jl
+++ src/basekernels/linear_spline.jl
@@ -8,7 +8,6 @@ end
 
 (k::LinearSplineKernel)(x::Real, y::Real) = 1 + k.c - (k.c / 10) * abs(x - y)
 
-
 # Specialised implementations required for Zygote performance.
 
 function kernelmatrix(k::LinearSplineKernel, x::AbstractVector{<:Real})
