d425983cd734c5c9c2e8c7573a97491ea5ecaa52
Will Tebbutt <wct23@cam.ac.uk>
2022-10-03 14:16:18 +0100

    Fix Matern Grads (#478)
    * gitignore dev folder
    
    * Fix up matern
    
    * Bump patch
    
    * Avoid promotion with irrational

--- src/basekernels/matern.jl
+++ src/basekernels/matern.jl
@@ -37,14 +37,16 @@ MaternKernel(; nu::Real=1.5, ν::Real=nu, metric=Euclidean()) = MaternKernel(ν,
 
 @functor MaternKernel
 
-@inline function kappa(k::MaternKernel, d::Real)
-    result = _matern(only(k.ν), d)
-    return ifelse(iszero(d), one(result), result)
-end
+@inline kappa(k::MaternKernel, d::Real) = _matern(only(k.ν), d)
 
 function _matern(ν::Real, d::Real)
-    y = sqrt(2ν) * d
-    return exp((one(d) - ν) * logtwo - loggamma(ν) + ν * log(y) + log(besselk(ν, y)))
+    if iszero(d)
+        return one(d)
+    else
+        y = sqrt(2ν) * d
+        b = log(besselk(ν, y))
+        return exp((one(d) - ν) * oftype(y, logtwo) - loggamma(ν) + ν * log(y) + b)
+    end
 end
 
 metric(k::MaternKernel) = k.metric
