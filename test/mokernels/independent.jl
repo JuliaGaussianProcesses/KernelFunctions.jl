f6f7d64f22cc31e251b038f525ad8a3cfc9e1fdf
Will Tebbutt <will.tebbutt@invenialabs.co.uk>
2023-02-27 14:54:15 +0000

    Loosen tolerance on test

--- test/mokernels/independent.jl
+++ test/mokernels/independent.jl
@@ -1,8 +1,9 @@
 @testset "independent" begin
     outdim = 3
-    x = KernelFunctions.MOInputIsotopicByOutputs([rand(5) for _ in 1:4], outdim)
-    y = KernelFunctions.MOInputIsotopicByOutputs([rand(5) for _ in 1:4], outdim)
-    z = KernelFunctions.MOInputIsotopicByOutputs([rand(5) for _ in 1:2], outdim)
+    rng = StableRNG(123456)
+    x = KernelFunctions.MOInputIsotopicByOutputs([rand(rng, 5) for _ in 1:4], outdim)
+    y = KernelFunctions.MOInputIsotopicByOutputs([rand(rng, 5) for _ in 1:4], outdim)
+    z = KernelFunctions.MOInputIsotopicByOutputs([rand(rng, 5) for _ in 1:2], outdim)
 
     xIF = KernelFunctions.MOInputIsotopicByFeatures(x.x, outdim)
     yIF = KernelFunctions.MOInputIsotopicByFeatures(y.x, outdim)
@@ -14,19 +15,22 @@
     @test k isa Kernel
     @test k.kernel isa Kernel
 
-    @test kernelmatrix(k, x, y) == kernelmatrix(k, collect(x), collect(y))
+    @test isapprox(
+        kernelmatrix(k, x, y), kernelmatrix(k, collect(x), collect(y));
+        rtol=1e-6,
+    )
 
     ## accuracy
     KernelFunctions.TestUtils.test_interface(k, x, y, z)
     KernelFunctions.TestUtils.test_interface(k, xIF, yIF, zIF)
 
     # type stability (maybe move to test_interface?)
-    x2 = MOInput(rand(Float32, 4), 2)
+    x2 = MOInput(rand(rng, Float32, 4), 2)
     @test k(x2[1], x2[2]) isa Float32
     @test k(x2[1], x2[1]) isa Float32
     @test eltype(typeof(kernelmatrix(k, x2))) <: Float32
 
     @test string(k) ==
         "Independent Multi-Output Kernel\n" *
-          "\tSquared Exponential Kernel (metric = Euclidean(0.0))"
+            "\tSquared Exponential Kernel (metric = Euclidean(0.0))"
 end
