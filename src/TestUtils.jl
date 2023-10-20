ab090024082890cc1b985353555e321c64ad5239
David Widmann <devmotion@users.noreply.github.com>
2023-10-20 10:25:06 +0200

    Improve error message

--- src/TestUtils.jl
+++ src/TestUtils.jl
@@ -5,14 +5,17 @@ function test_with_type end
 function test_type_stability end
 function example_inputs end
 
-function __init__()
-    # Better error message if users forget to load Test
-    Base.Experimental.register_error_hint(MethodError) do io, exc, _, _
-        if exc.f === test_interface ||
-            exc.f === test_with_type ||
-            exc.f === test_type_stability ||
-            exc.f === example_inputs
-            print(io, "\\nDid you forget to load Test?")
+if isdefined(Base, :get_extension) && isdefined(Base.Experimental, :register_error_hint)
+    function __init__()
+        # Better error message if users forget to load Test
+        Base.Experimental.register_error_hint(MethodError) do io, exc, _, _
+            if (exc.f === test_interface ||
+                exc.f === test_with_type ||
+                exc.f === test_type_stability ||
+                exc.f === example_inputs) &&
+                (Base.get_extension(Distributions, :DistributionsTestExt) === nothing)
+                print(io, "\nDid you forget to load Test?")
+            end
         end
     end
 end
